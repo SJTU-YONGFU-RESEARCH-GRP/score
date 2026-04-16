1b02441937a1e366041ea13326200536a68f20ef
yongfu <yongfu.li@sjtu.edu.cn>
2026-04-16 10:04:11 +0800

    update

---
+++ datasets/ibex/eede2fbb/rtl_designs/maxperf/riscv-compliance/maxperf_riscv-compliance_tb.sv
@@ -0,0 +1,10 @@
+`timescale 1ns/1ps
+module maxperf_riscv-compliance_tb;
+  reg clk = 1'b0;
+  reg rst_n = 1'b0;
+  always #5 clk = ~clk;
+  initial begin
+    #20 rst_n = 1'b1;
+    #100 $finish;
+  end
+endmodule
