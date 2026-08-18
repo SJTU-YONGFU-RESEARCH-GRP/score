511d7d78acf3f40ceedeae90daeda9f549ed43a8
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:41:16 +0800

    t_param_default_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_default_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2003 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module m #(
+    parameter int Foo
+);
+endmodule
+
+module t;
+
+  m foo ();
+
+endmodule
