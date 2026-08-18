bc42f3e38273ab9dcca8af443ce8e149b67107d3
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:00:00 +0800

    t_tri_pullvec_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_tri_pullvec_bad.v
@@ -0,0 +1,23 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2018 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  tri [3:0] w;
+
+  pullup p0 (w[0]);
+  pulldown p1 (w[1]);
+  pulldown p2 (w[2]);
+  pullup p3 (w[3]);
+
+  always_ff @(posedge clk) begin
+    if (w != 4'b1001) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
