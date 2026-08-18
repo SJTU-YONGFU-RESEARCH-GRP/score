c69285611442efa5be35cb525db398f2efb6edea
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:08:55 +0800

    t_clocking_unsup1.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_clocking_unsup1.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  clocking cb @(posedge clk);
+    output posedge #1 a;
+    output negedge #1 b;
+    output edge #1 b;
+  endclocking
+
+endmodule
