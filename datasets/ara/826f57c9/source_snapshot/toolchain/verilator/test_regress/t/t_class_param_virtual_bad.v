bb45f93f09d933a000b17488cf63e15e0f936a5e
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:07:24 +0800

    t_class_param_virtual_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_param_virtual_bad.v
@@ -0,0 +1,30 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+virtual class VBase;
+endclass
+
+class Cls #(
+    parameter type T = VBase
+);
+  T t;
+  function new;
+    t = new;
+  endfunction
+endclass
+
+virtual class ClsVirt #(
+    parameter type T
+);
+endclass
+
+module t;
+  initial begin
+    automatic Cls c = new;  // Error
+    automatic ClsVirt #(VBase) cv = new;  // Error
+    $stop;
+  end
+endmodule
