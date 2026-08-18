c4c3d3d7609233fa9424bef04c12dddf946ff564
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:44:06 +0800

    t_preproc_defarg_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_preproc_defarg_bad.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2010 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+//See bug289
+// verilog_format: off
+`define A1(x)
+`define A2(x,y)
+
+`A1
+`A1(1,2)
+`A2
+`A2(1)
+`A2(1,2,3)
+
+module t;
+endmodule
