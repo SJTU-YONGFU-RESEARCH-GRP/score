d3c206a1609084cc082910c6687872047cadd1e9
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:32:34 +0800

    t_lint_import_name_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_import_name_bad.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2018 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+package defs;
+  int sig;
+endpackage
+
+import defs::sigs;
+
+module t;
+endmodule
