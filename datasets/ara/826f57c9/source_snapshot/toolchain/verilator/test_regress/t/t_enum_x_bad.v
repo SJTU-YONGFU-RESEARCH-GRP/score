6bb28a5bcf3bb048f2133725fe8eefef9b7ecf34
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:16:39 +0800

    t_enum_x_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_enum_x_bad.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2009 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  enum bit [1:0] {BADX = 2'b1x} BAD1;
+
+  enum logic [3:0] {
+    e0 = 4'b1xx1,
+    e1
+  } BAD2;
+
+  initial begin
+    $stop;
+  end
+
+endmodule
