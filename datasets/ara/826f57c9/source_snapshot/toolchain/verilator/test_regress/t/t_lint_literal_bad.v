4341ba3e175b4c6ab09b3b668b2719155f157d4b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:33:02 +0800

    t_lint_literal_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_literal_bad.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2017 Todd Strader
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+);
+
+  localparam the_localparam = 8'd256;
+
+endmodule
