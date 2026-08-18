e6335cc21b763b2f0c80772fba59929c88ecfb66
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:36:25 +0800

    t_math_shortreal_unsup_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_math_shortreal_unsup_bad.v
@@ -0,0 +1,13 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2016 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  shortreal s;
+
+  initial s = 1.2345;
+
+endmodule
