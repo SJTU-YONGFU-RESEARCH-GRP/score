cb6643c497d494788cf4e7cdd3c8d03abc9205d7
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:42:09 +0800

    t_param_type_bad2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_type_bad2.v
@@ -0,0 +1,10 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  localparam t = logic;  // Bad
+  localparam t2 = realtime;  // Bad
+endmodule
