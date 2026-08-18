d126531ce3b78c731a7de8229d1a4980b84f8526
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:45:14 +0800

    t_program.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_program.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2009 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+program t;
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endprogram
