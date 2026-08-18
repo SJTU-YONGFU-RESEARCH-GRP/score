bd3b9c27d0d453edc22e272ec1fad7b295fd8969
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:26:25 +0800

    t_inst_2star_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_inst_2star_bad.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  wire foo;
+  wire bar;
+
+  sub sub (.*, .*);
+
+  sub sub (foo, .*);
+
+  sub sub (foo, .bar);
+
+endmodule
+
+module sub (input foo, input bar);
+endmodule
