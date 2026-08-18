097fa37a7b1c1a9efc69c7e736b256346cfe03ea
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:37:25 +0800

    t_mem_trace_split.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_mem_trace_split.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Demonstrate complex user typea problem with --x-assign
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  logic [31:0] mem_a[32];
+  logic [15:0] mem_b[32];
+
+  int cyc = 0;
+
+  // finish report
+  always @(posedge clk) begin
+    cyc <= cyc + 1;
+    mem_a[cyc] <= cyc;
+    mem_b[cyc] <= 16'(cyc);
+    if (cyc == 10) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+
+
+endmodule
