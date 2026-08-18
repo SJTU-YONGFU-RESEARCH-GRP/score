1e38d89886ef168f8a74685ff03d5ed9ef6bb9da
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:31:08 +0800

    t_langext_order_sub.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_langext_order_sub.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// A test of the +verilog2001ext+ and +verilog2005ext+ flags.
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// verilator lint_off SYMRSVDWORD
+
+module t_langext_order_sub(input do);
+endmodule
