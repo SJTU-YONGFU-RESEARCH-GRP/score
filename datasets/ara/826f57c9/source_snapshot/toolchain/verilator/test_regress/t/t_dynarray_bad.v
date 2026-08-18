2cfb165ba563eda67d978d0bb1384350be3330a3
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:15:45 +0800

    t_dynarray_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_dynarray_bad.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  integer a[];
+
+  string s;
+
+  initial begin
+    s = "str";
+    a = new[s];  // Bad
+  end
+
+endmodule
