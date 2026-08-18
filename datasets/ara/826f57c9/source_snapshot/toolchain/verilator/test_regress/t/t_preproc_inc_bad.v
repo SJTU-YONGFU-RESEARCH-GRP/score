a3d5e90d9684732c688380c997298fbc4647b916
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:44:26 +0800

    t_preproc_inc_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_preproc_inc_bad.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2010 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+//See bug289
+
+`include "t_preproc_inc_inc_bad.vh"
+
+module t;
+endmodule
