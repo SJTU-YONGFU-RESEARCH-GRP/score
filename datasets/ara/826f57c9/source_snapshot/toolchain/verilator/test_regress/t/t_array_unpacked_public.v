361824fbb3d2a727f0c2473532c21483a3db03a7
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:01:50 +0800

    t_array_unpacked_public.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_array_unpacked_public.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Stefan Wallentowitz
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  logic din[0:15];
+
+  array_test array_test_inst (.din(din));
+
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
+
+module array_test (
+    input din[0:15]
+);
+endmodule
