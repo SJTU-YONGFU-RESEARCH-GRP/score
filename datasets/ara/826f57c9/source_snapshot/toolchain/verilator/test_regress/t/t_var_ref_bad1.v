6eb9776f58c142294fb28a6f14d8878d3bbfa0a4
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:03:48 +0800

    t_var_ref_bad1.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_var_ref_bad1.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2018 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// Make sure type errors aren't suppressable
+// verilator lint_off WIDTH
+
+module t;
+
+  bit bad_parent;
+  sub sub (.bad_sub_ref(bad_parent));  // Type mismatch
+
+endmodule
+
+module sub (
+    ref real bad_sub_ref
+);
+endmodule
