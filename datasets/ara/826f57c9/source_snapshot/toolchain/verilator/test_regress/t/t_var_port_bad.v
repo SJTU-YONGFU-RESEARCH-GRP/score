f2461fd98ef9246fa1f5f340ba0daa5715bf1cd1
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:03:43 +0800

    t_var_port_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_var_port_bad.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2008 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  subok subok (
+      .a(1'b1),
+      .b(1'b0)
+  );
+  sub sub (
+      .a(1'b1),
+      .b(1'b0)
+  );
+endmodule
+
+module subok (
+    input a,
+    b
+);
+endmodule
+
+module sub (
+    a
+);
+  input a, b;
+endmodule
