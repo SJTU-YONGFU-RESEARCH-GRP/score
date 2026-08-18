081acd24d00dd807c99db8523f09f4a09983b10d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:15:46 +0800

    t_dynarray_bits.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_dynarray_bits.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  integer a[];
+
+  initial begin
+    if ($bits(a) != 0) $stop;
+    a = new[10];
+    if ($bits(a) != 10 * 32) $stop;
+  end
+
+endmodule
