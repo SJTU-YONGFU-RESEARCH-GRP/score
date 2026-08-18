6697a2bc5d9c577876b404d46f32f186de08c001
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:16:36 +0800

    t_enum_type_nomethod_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_enum_type_nomethod_bad.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  typedef enum [3:0] {
+                      E01 = 1
+                      } my_t;
+
+  my_t e;
+
+  initial begin
+    e.bad_no_such_method();
+    $stop;
+  end
+
+endmodule
