27b310655916bc72dc58a3a5aa8af547b8d5b50b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:02:52 +0800

    t_var_bad_hide_docs.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_var_bad_hide_docs.v
@@ -0,0 +1,9 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2009 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  integer t;  //<--- Warning ('t' hidden by module 't')
+endmodule
