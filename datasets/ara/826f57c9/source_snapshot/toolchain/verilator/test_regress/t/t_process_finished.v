94029634458c557967e4b6b4e4a83456c16a2589
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:45:00 +0800

    t_process_finished.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_process_finished.v
@@ -0,0 +1,22 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  process p;
+
+  initial begin
+    p = process::self();
+  end
+
+  always @(posedge clk) begin
+    if (p.status() != process::FINISHED) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
