391f5772b71a82fbecda3eb97a0b83ad068afcbc
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:16:09 +0800

    t_enum_bad_wrap.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_enum_bad_wrap.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2003 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  typedef enum [1:0] {
+                  PREWRAP = 2'd3,
+                  WRAPPED
+                  } wrap_t;
+
+endmodule
