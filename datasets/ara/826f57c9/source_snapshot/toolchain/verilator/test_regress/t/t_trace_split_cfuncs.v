b0678b5b2e9e9a47547dc6b904703ca3a4955d7b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:58:46 +0800

    t_trace_split_cfuncs.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_split_cfuncs.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Varun Koyyalagunta
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars();
+  end
+
+endmodule
