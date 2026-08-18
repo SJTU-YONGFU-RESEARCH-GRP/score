eac5edf42a1039d34d78b0e00bc60bb35885b478
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:06:49 +0800

    t_wire_self_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_wire_self_bad.v
@@ -0,0 +1,13 @@
+// DESCRIPTION: Verilator: Verilog Test module for SystemVerilog 'alias'
+//
+// Simple bi-directional alias test.
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  wire myself = myself;
+
+endmodule
