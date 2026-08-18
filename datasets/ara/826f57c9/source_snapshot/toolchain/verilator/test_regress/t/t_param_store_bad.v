7c220be391ffb53e6e3861b4e20d650645ea323b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:42:00 +0800

    t_param_store_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_store_bad.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t #(
+    string S = "<unset>"
+);
+
+  initial begin
+    $value$plusargs("S=%s", S);  // BAD assignment to S
+    #1;  // Original bug got compile time error only with this line
+    $display("S=%s", S);
+    $finish;
+  end
+
+endmodule
