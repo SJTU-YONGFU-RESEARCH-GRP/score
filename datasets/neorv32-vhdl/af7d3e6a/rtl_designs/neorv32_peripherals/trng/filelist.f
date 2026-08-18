9637981b843d950e22f4822eaa8179fe8e9e2ac6
yongfu <liyongfu.sg@gmail.com>
2026-08-18 20:41:49 +0800

    filelist.f

---
+++ datasets/neorv32-vhdl/af7d3e6a/rtl_designs/neorv32_peripherals/trng/filelist.f
@@ -0,0 +1,2 @@
+/home/yongfu/proj/score/datasets/neorv32-vhdl/af7d3e6a/rtl_designs/neorv32_peripherals/trng/neorv32_trng.vhd
+/home/yongfu/proj/score/datasets/neorv32-vhdl/af7d3e6a/rtl_designs/neorv32_peripherals/trng/neorv32_package.vhd
