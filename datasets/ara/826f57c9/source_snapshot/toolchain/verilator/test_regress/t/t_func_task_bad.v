ccb270c3f32ba1639a923e9db0661f46b69f9b79
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:22:48 +0800

    t_func_task_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_func_task_bad.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2011 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  initial begin
+    if (a_task(1'b0)) $stop;  // <--- Bad: Calling task _as_ function
+  end
+
+  task a_task;
+    input ign;
+  endtask
+
+endmodule
