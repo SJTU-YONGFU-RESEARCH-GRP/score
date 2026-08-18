c436a1a3610a98c18c9bfe194231b496bb21e708
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:04:14 +0800

    t_var_xref_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_var_xref_bad.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  task tsk;
+  endtask
+
+  initial tsk.bad_missing_ref = 0;
+endmodule
