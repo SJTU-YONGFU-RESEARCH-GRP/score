4fec56c591232d38124f54831a798dc816f29bb9
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:05:30 +0800

    t_class_extends_vsyment.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_extends_vsyment.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+class Foo;
+endclass
+
+class Bar extends Foo;
+  int m_field = get_1();
+  function int get_1();
+    return 1;
+  endfunction
+endclass
