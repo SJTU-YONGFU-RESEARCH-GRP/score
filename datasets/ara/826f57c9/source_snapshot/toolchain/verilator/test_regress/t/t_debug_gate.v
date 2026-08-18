9789ac1b99ed72109d5942347fbce57a2196d2fc
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:12:17 +0800

    t_debug_gate.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_debug_gate.v
@@ -0,0 +1,31 @@
+// DESCRIPTION: Verilator: Dotted reference that uses another dotted reference
+// as the select expression
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (  /*AUTOARG*/
+    // Outputs
+    o,
+    // Inputs
+    i
+);
+  input i;
+  output o;
+  sub sub (
+      .i,
+      .o
+  );
+endmodule
+
+module sub (  /*AUTOARG*/
+    // Outputs
+    o,
+    // Inputs
+    i
+);
+  input i;
+  output o;
+  assign o = !i;
+endmodule
