cd3491ad77372592c875ce1df18b14317ea9db1b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:04:21 +0800

    t_verilated_header.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_verilated_header.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+`include "verilated.v"
+
+module t;
+
+  initial begin
+    `verilator_file_descriptor i;
+    `coverage_block_off
+    i = $fopen("/dev/null", "r");
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
