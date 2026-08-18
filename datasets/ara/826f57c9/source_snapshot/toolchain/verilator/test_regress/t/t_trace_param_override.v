2d1d1dcf34cd9bbe7939a0927ef4613bf789481f
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:58:25 +0800

    t_trace_param_override.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_param_override.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+`define STRINGIFY(x) `"x`"
+
+module t #(
+    parameter int POVERRODE = 16,
+    parameter int PORIG = 16
+);
+
+  initial begin
+    $dumpfile(`STRINGIFY(`TEST_DUMPFILE));
+    $dumpvars;
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
