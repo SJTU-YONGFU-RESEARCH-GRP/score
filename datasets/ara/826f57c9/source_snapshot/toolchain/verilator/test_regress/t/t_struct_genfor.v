d1d6c9ad981c86a43b0f0390782d0d78c00c2e3a
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:51:53 +0800

    t_struct_genfor.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_struct_genfor.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2013 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  for (genvar g = 0; g < 2; ++g) begin : genfor
+    typedef struct packed {
+      logic [31:0] val1;
+      logic [31:0] val2;
+    } struct_t;
+    struct_t forvar;
+
+    initial begin
+      forvar.val1 = 1;
+      forvar.val2 = 2;
+      if (forvar.val1 != 1) $stop;
+      if (forvar.val2 != 2) $stop;
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+endmodule
