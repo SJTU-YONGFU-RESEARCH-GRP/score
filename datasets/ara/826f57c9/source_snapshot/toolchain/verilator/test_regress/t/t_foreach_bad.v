29515668c3e4441af6cb371c5f5ca32e4f4c8721
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:20:30 +0800

    t_foreach_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_foreach_bad.v
@@ -0,0 +1,24 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  integer a, b;
+
+  reg [2:0][2:0] array;
+
+  initial begin
+    foreach (array);  // no index
+
+    foreach (array.array[a]);  // not supported
+
+    foreach (array[a.b]);  // no index
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
