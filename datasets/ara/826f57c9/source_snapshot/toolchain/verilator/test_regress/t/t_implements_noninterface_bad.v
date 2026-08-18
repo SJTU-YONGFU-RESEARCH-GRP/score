c34ca31bf4424bdc6d9176fd078411dfa8b0e81d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:43 +0800

    t_implements_noninterface_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_implements_noninterface_bad.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class NotIcls;
+endclass
+
+class ClsBad1 implements NotIcls;
+endclass
+
+interface class Icls;
+endclass
+
+class ClsBad2 extends Icls;
+endclass
+
+module t;
+  ClsBad2 c;
+endmodule
