464a55765ff05013250a766aeca07a2f8134472e
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:31:25 +0800

    t_lib_prot_inout_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lib_prot_inout_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Todd Strader
+// SPDX-License-Identifier: CC0-1.0
+
+module secret_impl (
+    input a,
+    input oe,
+    inout z,
+    output y
+);
+
+  assign z = oe ? a : 1'bz;
+  assign y = z;
+
+endmodule
