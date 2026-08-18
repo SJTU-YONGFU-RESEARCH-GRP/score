3e068f089e3891ecfe3af3ad424f8a4ce1fac0ff
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:02:49 +0800

    t_var_bad_hide.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_var_bad_hide.v
@@ -0,0 +1,27 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2003 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  // Check that the lint_on is obeyed.
+  // verilator lint_off VARHIDDEN
+  // verilator lint_on  VARHIDDEN
+
+  integer top;
+
+  task x;
+    output top;
+    begin
+    end
+  endtask
+
+  initial begin
+    begin : lower
+      integer top;
+    end
+  end
+
+endmodule
