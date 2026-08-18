8b06ccb63392286c69cb306c76fa8f829709ef4d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:18:14 +0800

    t_flag_libinc.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_flag_libinc.v
@@ -0,0 +1,27 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2005 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module liblib_a;
+  liblib_b b ();
+endmodule
+
+module liblib_b;
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
+
+module liblib_c;
+  // Unused
+  initial $stop;
+  liblib_d d ();
+endmodule
+
+module liblib_d;
+  // Unused
+  initial $stop;
+endmodule
