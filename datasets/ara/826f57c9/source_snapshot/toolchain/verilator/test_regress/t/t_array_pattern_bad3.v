1646310ed6f5ae78dfa209b8bb396716ce18ba80
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:01:39 +0800

    t_array_pattern_bad3.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_array_pattern_bad3.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2018 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// bug1364
+
+module t (
+    input clk,
+    input res
+);
+
+  int arr[3];
+  initial begin
+    arr = '{
+        default: '0,  //
+        1: '0,  //
+        1: '1
+    };  // Bad
+    arr = '{'0, '1, '0, '1};  // Bad, too many
+    arr = '{'0, '1};  // Bad, too few
+  end
+
+endmodule
