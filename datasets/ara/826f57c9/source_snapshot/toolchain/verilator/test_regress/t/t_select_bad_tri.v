2f9a109f4cbccb74c117a973cc0f7b587e8e8a98
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:49:25 +0800

    t_select_bad_tri.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_select_bad_tri.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2008 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  reg [72:1] in;
+  initial begin
+    if (in[((1'h0/1'b0))+:71] != 71'h0) $stop;
+  end
+
+endmodule
