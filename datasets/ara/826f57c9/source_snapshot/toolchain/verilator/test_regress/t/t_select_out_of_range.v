ff7910bffbe5ca135088f3239fea878888d26f21
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:49:41 +0800

    t_select_out_of_range.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_select_out_of_range.v
@@ -0,0 +1,27 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module serial_adder (
+    input cin,
+    output cout
+);
+  localparam WIDTH = 8;
+
+  wire [WIDTH:0] c;
+
+  generate
+    for (genvar i = 0; i < WIDTH; i++) full_adder fa (c[i+1]);
+  endgenerate
+
+  assign c[0] = cin;
+  assign cout = c[WIDTH+1];  // intentional out-of-range
+
+endmodule
+
+module full_adder (
+    output cout
+);
+endmodule
