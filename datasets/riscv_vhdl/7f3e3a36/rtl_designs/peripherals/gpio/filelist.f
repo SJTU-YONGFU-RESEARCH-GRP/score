557c7c709683a8153f03f240f020d203f589f594
yongfu <liyongfu.sg@gmail.com>
2026-08-18 22:08:30 +0800

    filelist.f

---
+++ datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/gpio/filelist.f
@@ -0,0 +1,2 @@
+/home/yongfu/proj/score/datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/gpio/axi4_gpio.vhd
+/home/yongfu/proj/score/datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/gpio/types_common.vhd
