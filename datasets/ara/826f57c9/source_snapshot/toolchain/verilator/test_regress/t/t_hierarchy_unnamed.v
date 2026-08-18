59301efe11260b64560c3c022c80d2a7460e3662
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:18 +0800

    t_hierarchy_unnamed.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_hierarchy_unnamed.v
@@ -0,0 +1,29 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2012 Chandan Egbert
+// SPDX-License-Identifier: CC0-1.0
+
+module sub ();
+endmodule
+
+module t (
+    input logic a,
+    input logic b,
+    output logic x,
+    output logic y
+);
+
+  always_comb begin
+    integer i;
+    x = a;
+  end
+
+  sub u0 ();
+
+  always_comb begin
+    integer j;
+    y = b;
+  end
+
+endmodule
