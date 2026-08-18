15bbb2bb5804c30161e6eb7e7db712c115bb1426
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:33:20 +0800

    t_lint_numwidth.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_numwidth.v
@@ -0,0 +1,9 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2010 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+logic [65535:0] a = 65536'd1;
+logic [65536:0] b = 65537'd1;
+logic [131071:0] c = 131072'd1;
