9d913d92e87b91f04ed676724e9b99be543fc042
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:58:05 +0800

    t_trace_no_top_name.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_no_top_name.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+`define STRINGIFY(x) `"x`"
+
+module t;
+  wire a = 0;
+  initial begin
+    $dumpfile(`STRINGIFY(`TEST_DUMPFILE));
+    $dumpvars;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
+
+module another_top;
+  wire b = 0;
+endmodule
