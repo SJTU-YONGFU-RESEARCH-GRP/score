d80a7b6e14007091f6be62e5b692a72bce48db37
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:13:51 +0800

    t_display_qqq.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_display_qqq.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  initial begin
+    $display("""First "quoted"\nsecond\
+third
+fourth""");
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
