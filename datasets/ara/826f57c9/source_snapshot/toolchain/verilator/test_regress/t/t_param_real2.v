2f135496fc026db5ed6a8d273e581fa61d40656d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:41:49 +0800

    t_param_real2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_real2.v
@@ -0,0 +1,36 @@
+// DESCRIPTION: Verilator: Verilog Test module
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module foo #(
+    parameter real BAR = 2.0
+) ();
+
+endmodule
+
+module t;
+
+  genvar m, r;
+  generate
+    for (m = 10; m <= 20; m += 10) begin : gen_m
+      for (r = 0; r <= 1; r++) begin : gen_r
+        localparam real LPARAM = m + (r + 0.5);
+        initial begin
+          if (LPARAM != foo_inst.BAR) begin
+            $display("%m: LPARAM != foo_inst.BAR (%f, %f)", LPARAM, foo_inst.BAR);
+            $stop();
+          end
+        end
+
+        foo #(.BAR(LPARAM)) foo_inst ();
+      end
+    end
+  endgenerate
+
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
