70822f514cf2ba520b601aa12870c5c4dbddecd2
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:50:39 +0800

    t_std_pkg_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_std_pkg_bad.v
@@ -0,0 +1,11 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+package std;
+endpackage
+
+module t;
+endmodule
