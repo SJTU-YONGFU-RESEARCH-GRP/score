6a281cdb4ba4ae07529888ae4c3e8e0b33c1e873
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:40:42 +0800

    t_package_import_bad2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_package_import_bad2.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+package Pkg1;
+endpackage
+
+package Pkg10;
+  // verilator lint_off PKGNODECL
+  import Pkg1b::*;  // BAD - typo in package name
+endpackage
+
+module t;
+endmodule
