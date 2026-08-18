d50fb06a37e1ca4865413cf50c56a86aa4582871
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:31:54 +0800

    t_lint_declfilename.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_declfilename.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2010 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  t_lint_declfilename sub ();
+endmodule
+
+module t_lint_declfilename;
+endmodule
