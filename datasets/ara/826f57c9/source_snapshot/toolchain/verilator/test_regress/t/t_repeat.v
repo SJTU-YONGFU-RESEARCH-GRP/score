261e1a8328012640e15f7f04088ed411a4886f0a
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:48:19 +0800

    t_repeat.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_repeat.v
@@ -0,0 +1,33 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2009 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  reg signed [2:0] negcnt;
+  integer times;
+  initial begin
+    times = 0;
+    repeat (1) begin
+      repeat (0) $stop;
+      repeat (-1) $stop;
+      negcnt = 'sb111;
+      // Not all commercial simulators agree on the below stopping or not
+      // verilator lint_off WIDTH
+      repeat (negcnt) $stop;
+      // verilator lint_on  WIDTH
+      repeat (5) begin
+        repeat (2) begin
+          times = times + 1;
+        end
+      end
+    end
+    if (times != 10) $stop;
+    //
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
