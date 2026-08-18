c70458ce8df61e820a650ecda66a6dfe2373df2e
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:10:51 +0800

    t_constraint_pure.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_constraint_pure.v
@@ -0,0 +1,26 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+virtual class Base;
+   pure constraint raint;
+endclass
+
+class Cls extends Base;
+   rand int b2;
+   constraint raint { b2 == 5; }
+endclass
+
+virtual class Virt extends Base;
+  // No constraint needed
+endclass
+
+module t;
+  initial begin
+    automatic Cls c = new;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
