1328fa54b0e3d7ef57622a77bd8f738eda2ff6d5
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:26:00 +0800

    t_initial_dlyass.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_initial_dlyass.v
@@ -0,0 +1,31 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2012 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  integer cyc;
+  initial cyc = 0;
+  integer a;
+  integer b;
+
+  initial begin
+    a <= 22;
+    b <= 33;
+  end
+
+  always @(posedge clk) begin
+    cyc <= cyc + 1;
+    if (cyc == 99) begin
+      if (a != 22) $stop;
+      if (b != 33) $stop;
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+
+endmodule
