02ec2b9829373dcef40376be9bc9e9f4fbaca5ea
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:53 +0800

    t_infinite_recursion.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_infinite_recursion.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain
+// SPDX-FileCopyrightText: 2024 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+class cls;
+  task t;
+    t;
+  endtask
+  task pre_randomize;
+    t;
+  endtask
+endclass
+module t;
+  cls obj;
+  task static t;
+    int _ = obj.randomize() with {1 == 1;};
+  endtask
+endmodule
