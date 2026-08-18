b198065abe89a7844a923eb2c6a86f5996c90bd7
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:31:48 +0800

    t_lint_comb_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_comb_bad.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2017 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (/*AUTOARG*/
+  // Inputs
+  clk
+  );
+
+  input clk;
+
+  always_comb @(*) begin
+    $stop;
+  end
+
+endmodule
