65d4943726032908519b16b8cca10d6155fe78d0
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:04:16 +0800

    t_varref_scope_in_interface.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_varref_scope_in_interface.v
@@ -0,0 +1,24 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface iface #(
+    parameter DWIDTH = 32
+) ();
+  localparam TOTAL_PACKED_WIDTH = DWIDTH + 1;
+  modport Tx(output sop, data, import unpack);
+  logic sop;
+  logic [DWIDTH - 1:0] data = '0;
+
+  task static unpack(input logic [TOTAL_PACKED_WIDTH-1:0] packed_in, input logic sop_i);
+    logic sop_nc;
+    {data, sop_nc} <= packed_in;
+    sop <= sop_i;
+  endtask
+endinterface
+
+module t;
+  iface ifc ();
+endmodule
