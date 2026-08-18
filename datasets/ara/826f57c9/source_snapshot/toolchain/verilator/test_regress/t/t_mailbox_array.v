2a4965d0bcee34dbe45050d95f25b0caf17d7cf3
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:35:12 +0800

    t_mailbox_array.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_mailbox_array.v
@@ -0,0 +1,26 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls;
+  localparam DWIDTH = 6;
+  typedef int my_type_t[2**DWIDTH];
+  mailbox #(my_type_t) m_mbx;
+
+  function new();
+    this.m_mbx = new(1);
+  endfunction
+endclass
+
+module tb_top ();
+  Cls c;
+  initial begin
+    c = new();
+    $display("%p", c);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
