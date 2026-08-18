24fb613ce6c9ef210fe1b765048ffc6fc795d920
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:04:02 +0800

    t_case_inside_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_case_inside_bad.v
@@ -0,0 +1,13 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  initial begin
+    casex (1'bx) inside
+      default: $stop;
+    endcase
+  end
+endmodule
