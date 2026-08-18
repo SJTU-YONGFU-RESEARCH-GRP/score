17907109ca88a167964b500a29764148ce73e909
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:17:03 +0800

    t_event_method_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_event_method_bad.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  event e1;
+
+  initial begin
+    e1.bad_method();
+  end
+
+endmodule
