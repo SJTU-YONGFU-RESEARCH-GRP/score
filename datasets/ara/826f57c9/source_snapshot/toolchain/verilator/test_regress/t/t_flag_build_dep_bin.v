bce213e13fe698ea0490d5660f4579ccbe38e70d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:17:26 +0800

    t_flag_build_dep_bin.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_flag_build_dep_bin.v
@@ -0,0 +1,8 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2005 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+endmodule
