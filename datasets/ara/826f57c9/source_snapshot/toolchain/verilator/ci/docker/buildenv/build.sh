f62e6f05ff9b8a12fb8f44ef06f3aa1621432b76
yongfu <liyongfu.sg@gmail.com>
2026-08-18 11:56:11 +0800

    build.sh

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/ci/docker/buildenv/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+# DESCRIPTION: Build Verilator (inside container)
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of either the GNU Lesser General Public License Version 3
+# or the Perl Artistic License Version 2.0.
+# SPDX-FileCopyrightText: 2020 Stefan Wallentowitz
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+: "${REPO:=https://github.com/verilator/verilator}"
+: "${REV:=master}"
+: "${CXX:=g++}"
+
+SRCS=$PWD/verilator
+
+git clone "$REPO" "$SRCS"
+cd "$SRCS"
+git checkout "$REV"
+autoconf
+./configure --enable-longtests
+make -j $(nproc)
+if [ "${1:-''}" == "test" ]; then
+    make test
+fi
