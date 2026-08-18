50a940ab992a5b7e32527f2636ba091d9faec2cb
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:40:33 +0800

    t_package_dup_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_package_dup_bad.v
@@ -0,0 +1,33 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+package pkg;
+  localparam PARAM = 10;
+endpackage
+
+package pkg;
+  localparam PARAM = 10;
+endpackage
+
+module sub
+  import pkg::*;
+#(
+) ();
+endmodule
+
+package pkg;
+endpackage
+
+package pkg;
+endpackage
+
+module t;
+  sub sub ();
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
