3ed6c0d5f4ec80636a275cc757bf2628309c8f97
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:47:06 +0800

    t_randomize_method_nclass_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_randomize_method_nclass_bad.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  initial begin
+    randomize(1);
+    srandom(1);
+  end
+endmodule
