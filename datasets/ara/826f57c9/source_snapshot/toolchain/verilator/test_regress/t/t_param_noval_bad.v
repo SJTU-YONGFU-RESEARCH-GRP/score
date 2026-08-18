21e6d39d1fb2d05511d995977ff880c482c30c26
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:41:39 +0800

    t_param_noval_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_noval_bad.v
@@ -0,0 +1,13 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t #(parameter P, parameter type T);
+  generate
+    var j;
+    for (j=0; P; j++)
+      initial begin end
+  endgenerate
+endmodule
