b6fe0612619c97ebc08455a568168012136b0f1c
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:03:34 +0800

    t_bitsel_struct.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_bitsel_struct.v
@@ -0,0 +1,29 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// A test case for struct signal bit selection.
+//
+// This test is to check that bit selection of multi-dimensional signal inside
+// of a struct works.
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2012 Jie Xu
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  typedef struct packed {
+    logic [1:0][15:0] channel;
+    logic others;
+  } buss_t;
+
+  buss_t b;
+  reg [7:0] a;
+
+  initial begin
+    b = {16'h8765, 16'h4321, 1'b1};
+    a = b.channel[0][8+:8];
+    if (a != 8'h43) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
