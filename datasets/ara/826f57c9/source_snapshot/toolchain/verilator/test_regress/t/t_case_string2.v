395a8d1d4c1e34043d42c4b085bd07219e0dcea2
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:04:11 +0800

    t_case_string2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_case_string2.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  function automatic string broken_case(input string some_string);
+    case (some_string)
+      "alpha": return "alpha";
+      default: return "beta";
+    endcase
+  endfunction
+
+  initial begin
+    $display(broken_case("gamma"));
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
