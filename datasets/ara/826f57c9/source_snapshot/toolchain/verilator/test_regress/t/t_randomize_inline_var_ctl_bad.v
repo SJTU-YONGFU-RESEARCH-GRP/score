ab2d076a35464b93f33831fec5c0a2156dcb1f80
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:46:53 +0800

    t_randomize_inline_var_ctl_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_randomize_inline_var_ctl_bad.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain
+// SPDX-FileCopyrightText: 2024 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+class Foo;
+  int x;
+
+  function void test;
+    int y;
+    void'(randomize(y));
+  endfunction
+endclass
+
+class Bar;
+  int y;
+endclass
+
+module t;
+  initial begin
+    automatic Foo foo = new;
+    automatic Foo qux = new;
+    automatic Bar bar = new;
+    automatic int x;
+    void'(foo.randomize(x, foo.x, null, qux.x, bar.y, 0 + 1, x ** 2));
+  end
+endmodule
