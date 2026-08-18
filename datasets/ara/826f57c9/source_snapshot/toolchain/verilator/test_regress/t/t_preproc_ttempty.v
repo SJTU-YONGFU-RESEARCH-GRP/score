0c6cb6063e53fa615fee330e43502e658a116475
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:44:48 +0800

    t_preproc_ttempty.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_preproc_ttempty.v
@@ -0,0 +1,9 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2017 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+//`define TARGET_PACKAGE
+
+`define TARGET_PACKAGE_```TARGET_PACKAGE
