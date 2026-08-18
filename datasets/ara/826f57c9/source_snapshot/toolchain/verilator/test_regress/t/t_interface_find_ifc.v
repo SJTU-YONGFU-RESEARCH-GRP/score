47068cc108484cbbdf2d58b8a18a78d4f6d4ccba
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:28:18 +0800

    t_interface_find_ifc.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_interface_find_ifc.v
@@ -0,0 +1,9 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2013 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface t_interface_find_ifc;
+  logic [3:0] value;
+endinterface
