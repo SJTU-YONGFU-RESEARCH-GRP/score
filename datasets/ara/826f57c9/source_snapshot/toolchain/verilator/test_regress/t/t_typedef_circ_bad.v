3efe5040d0ae8ff46f0dc22384dd0bcba22efe74
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:00:27 +0800

    t_typedef_circ_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_typedef_circ_bad.v
@@ -0,0 +1,9 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+typedef a_t;
+typedef a_t b_t;
+typedef b_t a_t;
