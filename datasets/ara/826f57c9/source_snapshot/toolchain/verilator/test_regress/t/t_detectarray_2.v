920f84b12c171a2210aeb78359bfc72166038b8a
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:12:35 +0800

    t_detectarray_2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_detectarray_2.v
@@ -0,0 +1,36 @@
+// DESCRIPTION: Verilator: Simple test of unoptflat
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2013 Jeremy Bennett
+// SPDX-License-Identifier: CC0-1.0
+
+localparam ID_MSB = 1;
+
+
+module t (
+    input clk
+);
+
+  typedef struct packed {logic [1:0] id;} context_t;
+
+  context_t tsb;
+
+  assign tsb.id = {tsb.id[0], clk};
+
+  initial begin
+    tsb.id = 0;
+  end
+
+  always @(posedge clk or negedge clk) begin
+
+`ifdef TEST_VERBOSE
+    $write("tsb.id = %x\n", tsb.id);
+`endif
+
+    if (tsb.id[1] != 0) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+
+endmodule
