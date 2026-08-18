f99362d0af767947e0fbaa4604130b38ce4f6b2c
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:43:33 +0800

    t_pp_circdef_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_pp_circdef_bad.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2011 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+//
+// bug445
+// verilog_format: off
+`define WIDTH  12
+`define SEL_NUM_BITS `WIDTH-`SEL_NUM_BITS +: `SEL_NUM_BITS
+`define SEL_BITS     `WIDTH-`SEL_NUM_BITS +: `SEL_NUM_BITS
+`define ADDR_BITS    0 +: `WIDTH-`SEL_NUM_BITS
+
+typedef logic [`SEL_NUM_BITS-1:0]  d_t;
