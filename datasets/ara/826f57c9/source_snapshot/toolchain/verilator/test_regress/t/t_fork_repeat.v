4b7d1b2f3db609cd456af1fcc03a6f52d753c8cf
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:21:11 +0800

    t_fork_repeat.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_fork_repeat.v
@@ -0,0 +1,30 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  bit clk;
+
+  // Gen Clock
+  always #10 clk = ~clk;
+
+  initial begin
+    fork
+      begin
+        forever @(posedge clk);
+      end
+      begin
+        repeat (10) @(posedge clk);
+      end
+      begin
+        for (int i = 0; i < 6; ++i) @(posedge clk);
+      end
+    join_any
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
