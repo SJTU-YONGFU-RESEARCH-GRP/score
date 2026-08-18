eb921875b21df1fe2d44ad13aeb320d792c2386a
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:43:37 +0800

    t_pp_defparen_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_pp_defparen_bad.v
@@ -0,0 +1,11 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// verilog_format: off
+`define test(a1,a2) ((a1) + (a2))
+
+`test val
+ ( 1,2)
