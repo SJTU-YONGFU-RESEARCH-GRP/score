a542c35f0d6d76978405e4f6ae3a8ae0c5d75487
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:46:54 +0800

    t_randomize_inline_var_ctl_unsup_1.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_randomize_inline_var_ctl_unsup_1.v
@@ -0,0 +1,24 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain
+// SPDX-FileCopyrightText: 2024 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+class Foo;
+  int x;
+
+  static function Foo get;
+    Foo foo = new;
+    return foo;
+  endfunction
+endclass
+
+module t;
+  initial begin
+    automatic Foo foo = Foo::get();
+    automatic Foo foos[] = new[1];
+    void'(foo.randomize(Foo::get().x));
+    void'(foo.randomize(foos[0].x));
+    void'(foo.randomize(null));
+  end
+endmodule
