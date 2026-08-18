6fd5e4b0acfad162528f77a368f774713669f1da
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:18:27 +0800

    t_flag_nomod_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_flag_nomod_bad.v
@@ -0,0 +1,7 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2008 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+`define EMPTY 1
