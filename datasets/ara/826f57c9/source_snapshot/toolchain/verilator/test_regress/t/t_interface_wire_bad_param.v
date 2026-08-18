308932322c0a15aa0f2f0cdab9325b3dd6f46b25
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:30:42 +0800

    t_interface_wire_bad_param.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_interface_wire_bad_param.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface Ifc;
+endinterface
+
+module Sub #(
+    parameter P
+);
+  Ifc a ();
+endmodule
+
+module t;
+  Sub #(0) sub ();
+  // Issue #5649
+  wire wbad = sub.a;
+endmodule
