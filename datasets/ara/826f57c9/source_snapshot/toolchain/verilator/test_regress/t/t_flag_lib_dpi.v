591a5f80c13f8a2f15cba82342733767947188fb
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:18:12 +0800

    t_flag_lib_dpi.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_flag_lib_dpi.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Shupei Fan
+// SPDX-License-Identifier: CC0-1.0
+
+import "DPI-C" function void write_all_finished();
+
+module t;
+
+  initial begin
+    write_all_finished;
+    $finish;
+  end
+
+endmodule
