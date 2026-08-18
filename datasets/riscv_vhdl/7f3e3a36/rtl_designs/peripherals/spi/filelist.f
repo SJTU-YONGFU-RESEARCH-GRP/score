bf8c8e8fc3145332b0ca922e1af67c17058b0394
yongfu <liyongfu.sg@gmail.com>
2026-08-18 22:08:37 +0800

    filelist.f

---
+++ datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/spi/filelist.f
@@ -0,0 +1,2 @@
+/home/yongfu/proj/score/datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/spi/axi4_flashspi.vhd
+/home/yongfu/proj/score/datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/spi/types_common.vhd
