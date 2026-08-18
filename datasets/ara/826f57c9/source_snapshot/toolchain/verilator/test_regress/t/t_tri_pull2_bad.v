cd96c12ab78bd140eb8533f1e1416a79c59054db
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:59:52 +0800

    t_tri_pull2_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_tri_pull2_bad.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2010 Lane Brooks
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  wire A;
+
+  pullup p1 (A);
+
+  child child (  /*AUTOINST*/
+      // Inouts
+      .A(A)
+  );
+
+endmodule
+
+module child (
+    inout A
+);
+
+  pulldown p2 (A);
+
+endmodule
