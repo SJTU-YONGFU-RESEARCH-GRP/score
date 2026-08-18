f759599d82c72dc2bc067cf3e21fbffd950c8d2f
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:22:06 +0800

    t_func_modify_input.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_func_modify_input.v
@@ -0,0 +1,13 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+class foo;
+  function void g(input integer x);
+    f(x);
+  endfunction
+  function void f(inout integer x);
+  endfunction
+endclass
