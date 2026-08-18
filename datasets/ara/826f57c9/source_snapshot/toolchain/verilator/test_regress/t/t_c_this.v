0509e51403a4a8c3e354343a7ec1201e42c12e94
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:03:39 +0800

    t_c_this.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_c_this.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2021 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    clk
+);
+  input clk;
+  always @(posedge clk) begin
+    $c("const CData xthis = this->clk;");
+    $c("const CData thisx = xthis;");
+    $c("const CData xthisx = thisx;");
+    $c("this->clk = xthisx;");
+  end
+endmodule
