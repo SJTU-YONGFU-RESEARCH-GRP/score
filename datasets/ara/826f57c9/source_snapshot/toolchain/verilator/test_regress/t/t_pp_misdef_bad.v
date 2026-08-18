03dfa85190b15dd77f7fd18ed44d6373af5cf97f
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:43:49 +0800

    t_pp_misdef_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_pp_misdef_bad.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2004 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// verilog_format: off
+module t;
+`define DEFINED
+
+   // NDEFINED isn't defined here:
+   `NDEFINED
+
+     // Botched directive (`timescale)
+     `imescale
+
+     initial $stop; // Should have failed
+
+endmodule
