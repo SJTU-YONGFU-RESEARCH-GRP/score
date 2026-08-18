32a928c77785d8701cb7bc231287930123b5ca65
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:48:52 +0800

    t_sc_names.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_sc_names.v
@@ -0,0 +1,11 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain
+// SPDX-FileCopyrightText: 2020 Edgar E. Iglesias
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    clk
+);
+  input clk;
+endmodule
