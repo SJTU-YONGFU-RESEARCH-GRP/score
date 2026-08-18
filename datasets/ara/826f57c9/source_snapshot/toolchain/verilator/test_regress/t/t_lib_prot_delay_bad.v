fa40fc29f75379b030679e1ae11d82d88135a228
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:31:23 +0800

    t_lib_prot_delay_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lib_prot_delay_bad.v
@@ -0,0 +1,11 @@
+// DESCRIPTION: Verilator: Verilog Test module
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Todd Strader
+// SPDX-License-Identifier: CC0-1.0
+
+module secret_impl;
+  initial begin
+    #10;
+    $stop;
+  end
+endmodule
