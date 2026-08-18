089c39a398db23b9afebdd51be052278685ed938
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:02:29 +0800

    t_assert_imm_nz_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_assert_imm_nz_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  input clk;
+
+  labeled_imas :
+  assert #1 (clk);  // BAD: #1
+
+endmodule
