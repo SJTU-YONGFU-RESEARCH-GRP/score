eafc5e48c4aa318c5ed0a74ddd2a20dd4bfc8b38
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:41:34 +0800

    t_param_mintypmax.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_mintypmax.v
@@ -0,0 +1,32 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  parameter MTM = (1: 2: 3);
+
+  sub sub ();
+  //UNSUP sub #(.MTM(10:20:30)) sub20name ();
+  //UNSUP sub #(.MTM(100:200)) sub200name ();
+  //UNSUP sub #(10:20:30) sub20pos ();
+  //UNSUP sub #(100:200) sub200pos ();
+
+  initial begin
+    if (MTM != 2) $stop;
+    //UNSUP if (sub20pos.MTM != 20) $stop;
+    //UNSUP if (sub200pos.MTM != 200) $stop;
+    //UNSUP if (sub20name.MTM != 20) $stop;
+    //UNSUP if (sub200name.MTM != 200) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
+
+module sub #(
+    parameter MTM = (1: 2: 3)
+) ();
+endmodule
