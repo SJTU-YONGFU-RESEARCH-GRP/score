e9ff5d593a8741994dfce29c183e99e2b0a6cc15
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:06:08 +0800

    t_vpi_release_dup_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_vpi_release_dup_bad.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under The Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+import "DPI-C" context function void dpii_check();
+
+module t;
+  initial begin
+    dpii_check();
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
