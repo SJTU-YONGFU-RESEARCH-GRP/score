647f1bf6259965d04716ac177001f979fa664cf9
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:07:02 +0800

    t_class_param_nconst_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_param_nconst_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls #(
+    parameter PARAM = 12
+);
+endclass
+
+module t;
+
+  Cls #(.PARAM($random)) c;  // Bad param name
+
+endmodule
