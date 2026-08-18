b31e7b875d5aedd2eb71dc7cde4c2e68219fa583
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:32:24 +0800

    t_lint_implicit.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_implicit.v
@@ -0,0 +1,22 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2008 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    a,
+    z
+);
+  input a;
+  output z;
+
+  assign b = 1'b1;
+
+  or OR0 (nt0, a, b);
+
+  logic [1:0] dummy_ip;
+  assign {dummy1, dummy2} = dummy_ip;
+
+  assign z = nt0;
+endmodule
