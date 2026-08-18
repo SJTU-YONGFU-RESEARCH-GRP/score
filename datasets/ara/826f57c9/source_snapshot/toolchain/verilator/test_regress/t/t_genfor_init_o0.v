22269244e6b2da95a84cb2cbfda71064c812199b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:24:39 +0800

    t_genfor_init_o0.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_genfor_init_o0.v
@@ -0,0 +1,11 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  genvar i;
+  for (i = 0; i < 0; i = i + 1) begin
+  end
+endmodule
