ece7f40134b1f30d0c787657b16a182e2fe8cf71
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:00:34 +0800

    t_typedef_id_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_typedef_id_bad.v
@@ -0,0 +1,10 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+int i;
+
+class Cls #(parameter type P_T = i);
+endclass
