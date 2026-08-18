3ce7ccb12997fe06ad8e84c6206694db816cd460
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:02:34 +0800

    t_assert_on.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_assert_on.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2007 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  always @(posedge clk) begin
+    assert (0);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
