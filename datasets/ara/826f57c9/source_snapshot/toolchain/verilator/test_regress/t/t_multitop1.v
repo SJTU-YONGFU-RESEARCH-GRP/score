9056351a4ab5156407a92ced56a589c60444ebc2
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:38:22 +0800

    t_multitop1.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_multitop1.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+  t_multitop1s s ();
+  initial $display("In '%m'");
+  always @(posedge clk) begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
