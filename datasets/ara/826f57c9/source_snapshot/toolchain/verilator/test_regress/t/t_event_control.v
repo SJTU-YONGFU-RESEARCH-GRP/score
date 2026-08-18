c68824163162201255ff0ee201bbcf98b614aece
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:16:47 +0800

    t_event_control.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_event_control.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2003 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  initial begin
+    @(clk);
+    $write("[%0t] Got\n", $time);
+    @(clk);
+    $write("[%0t] Got\n", $time);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
