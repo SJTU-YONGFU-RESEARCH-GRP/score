3319e657fea6d8f4ab225ef6adad3677cbd5a0c9
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:02:27 +0800

    t_unroll_pragma.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_unroll_pragma.v
@@ -0,0 +1,47 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// verilog_format: off
+`ifdef TEST_DISABLE
+ `define PRAGMA /*verilator unroll_disable*/
+`elsif TEST_FULL
+ `define PRAGMA /*verilator unroll_full*/
+`elsif TEST_NONE
+ `define PRAGMA
+`endif
+// verilog_format: on
+
+module t;
+
+  int i, j;
+
+  // This must always unroll
+  for (genvar g = 0; g < 10; ++g) begin
+    initial $c("gened();");
+  end
+
+  initial begin
+    // Test a loop equal to --unroll-count - should unroll without pragma
+    `PRAGMA
+    for (i = 0; i < 4; ++i) begin
+      `PRAGMA
+      for (j = 0; j < 4; ++j) begin
+        $c("small();");
+      end
+    end
+    // Test a loop larger than --unroll-count
+    `PRAGMA
+    for (i = 0; i < 5; ++i) begin
+      `PRAGMA
+      for (j = 0; j < 5; ++j) begin
+        $c("large();");
+      end
+    end
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
