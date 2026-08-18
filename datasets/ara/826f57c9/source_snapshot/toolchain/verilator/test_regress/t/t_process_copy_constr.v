2193c38818b924edbbc2a8c1d3b8cedbe542081e
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:44:59 +0800

    t_process_copy_constr.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_process_copy_constr.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain
+// SPDX-FileCopyrightText: 2024 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls;
+  int x = 1;
+  function new();
+    process p = process::self();
+  endfunction
+endclass
+
+module t (  /*AUTOARG*/
+);
+  initial begin
+    Cls c, d;
+    c = new;
+    c.x = 2;
+    d = new c;
+    if (d.x != 2) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
