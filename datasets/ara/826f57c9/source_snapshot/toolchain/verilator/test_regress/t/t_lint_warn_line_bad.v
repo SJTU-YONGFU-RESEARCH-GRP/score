6c1505b6a0cd8d3415a4ed2f6bb447c4ef87da9b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:34:14 +0800

    t_lint_warn_line_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_warn_line_bad.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// Check that `line `__LINE__ still shows proper warning context
+
+`line `__LINE__ "the_line_file" 1
+`line `__LINE__ "the_line_file" 2
+
+module t;
+  int warn_t = 64'h1;  // Not suppressed - should warn
+endmodule
