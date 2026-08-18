8d77b463e12dc39db44a16b4dac7e19e049b499b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:38:04 +0800

    t_mod_topmodule.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_mod_topmodule.v
@@ -0,0 +1,34 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This test verifies that a top-module can be specified which
+// is instantiated beneath another module in the compiled source
+// code.
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2021 Dan Petrisko
+// SPDX-License-Identifier: CC0-1.0
+
+module top(/*AUTOARG*/
+   // Inputs
+   clk
+   );
+   input clk;
+
+   always_ff @(posedge clk) begin
+      $write("*-* All Finished *-*\n");
+      $finish();
+   end
+
+endmodule
+
+module faketop(/*AUTOARG*/
+   );
+
+   top top();
+
+   // Stop immediately if this module is instantiated
+  initial begin
+    $stop();
+   end
+
+endmodule
