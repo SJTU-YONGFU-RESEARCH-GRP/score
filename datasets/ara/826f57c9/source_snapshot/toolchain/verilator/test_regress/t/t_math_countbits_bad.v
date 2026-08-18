282bae20ba81b8039effeedcf276647fc9eb0dea
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:35:32 +0800

    t_math_countbits_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_math_countbits_bad.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Yossi Nivin
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  integer count;
+  assign count = $countbits(32'h123456, '0, '1, 'x, 'z);
+
+endmodule
