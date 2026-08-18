fc2777f293bd439c344f0bb6328c412da282dc46
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:26:10 +0800

    t_inside_assoc_unsup.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_inside_assoc_unsup.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  int assoc[int];
+  bit m;
+
+  initial begin
+    m = (10 inside {assoc});
+  end
+
+endmodule
