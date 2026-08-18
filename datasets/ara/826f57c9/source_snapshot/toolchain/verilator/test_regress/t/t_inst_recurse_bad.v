ec1693b70854ec6bb8d9a59d595df3c43e7b5bfe
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:27:20 +0800

    t_inst_recurse_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_inst_recurse_bad.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2005 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  looped looped ();
+
+endmodule
+
+module looped;
+  looped2 looped2 ();
+endmodule
+
+module looped2;
+  looped looped ();
+endmodule
