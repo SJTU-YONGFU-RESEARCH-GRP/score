967b9c9d200a5c281568eee7f98f5c84fa869269
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:05:06 +0800

    t_class_enum.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_enum.v
@@ -0,0 +1,23 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  class Cls;
+    typedef enum {
+      A = 10,
+      B = 20,
+      C = 30
+    } en_t;
+  endclass
+
+  initial begin
+    Cls c;
+    if (c.A != 10) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
