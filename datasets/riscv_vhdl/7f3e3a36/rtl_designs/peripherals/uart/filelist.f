7d660ff1d02ee74d2096ced5185acdd9d0d2cb41
yongfu <liyongfu.sg@gmail.com>
2026-08-18 22:08:40 +0800

    filelist.f

---
+++ datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/uart/filelist.f
@@ -0,0 +1,2 @@
+/home/yongfu/proj/score/datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/uart/axi4_uart.vhd
+/home/yongfu/proj/score/datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/uart/types_common.vhd
