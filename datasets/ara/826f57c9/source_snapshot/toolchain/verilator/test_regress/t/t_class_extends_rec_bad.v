b32442ac59cbb4efb80bae092bb07a951b240dc6
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:05:27 +0800

    t_class_extends_rec_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_extends_rec_bad.v
@@ -0,0 +1,13 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class RecursiveExtCls extends RecursiveExtCls;
+  int i;
+endclass
+
+module t;
+  RecursiveExtCls cls = new;
+endmodule
