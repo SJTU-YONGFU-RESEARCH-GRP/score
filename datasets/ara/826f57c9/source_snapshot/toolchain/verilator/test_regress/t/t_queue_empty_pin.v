0c48c19502a159993d77b2f882deb31746de54e3
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:46:02 +0800

    t_queue_empty_pin.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_queue_empty_pin.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  task tsk(int q[] = {});
+    if (q.size != 0) $stop;
+  endtask
+
+  initial begin
+    tsk();
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
