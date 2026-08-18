597771304bfe0b59a716101f4e6c79b1f66669ca
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:06:57 +0800

    t_class_param_extra_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_param_extra_bad.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  class Cls1;
+    typedef bit bool_t;
+  endclass
+
+  localparam Cls1#(123, integer, "text")::bool_t PARAM = 1;
+
+endmodule
