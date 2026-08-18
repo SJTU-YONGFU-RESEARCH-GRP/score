84ef002f378ce54d85372fb7e77c058861416d70
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:16:04 +0800

    t_enum_bad_dup.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_enum_bad_dup.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2003 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  typedef enum {
+    DUP_VALUE = 2,
+    DUP_VALUE = 3
+  } dup_t;
+
+endmodule
