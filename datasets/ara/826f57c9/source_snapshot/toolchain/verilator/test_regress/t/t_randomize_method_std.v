0dbd0c88d48387b637bf5fdafa91200d3ffd7751
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:47:08 +0800

    t_randomize_method_std.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_randomize_method_std.v
@@ -0,0 +1,22 @@
+// DESCRIPTION: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+process p;  // force importing std into top-level namespace
+
+class C;
+  function new;
+    if (randomize() != 1) $stop;
+  endfunction
+endclass
+
+module t;
+  initial begin
+    automatic C c = new;
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
