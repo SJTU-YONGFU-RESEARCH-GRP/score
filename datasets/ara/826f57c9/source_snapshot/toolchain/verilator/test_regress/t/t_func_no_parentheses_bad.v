a2f3decd50dae434af43cc226d689bc766e1aeed
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:22:14 +0800

    t_func_no_parentheses_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_func_no_parentheses_bad.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+function static int func();
+  int cnt = 0;
+  return ++cnt;
+endfunction
+
+module t;
+
+  int a;
+  initial begin
+    a = func;
+    $stop;
+  end
+
+endmodule
