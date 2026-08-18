f9e0f7718ebd1f852c4595d6b8420a10e79c059d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:38:24 +0800

    t_multitop_sig.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_multitop_sig.v
@@ -0,0 +1,42 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module a (
+    in,
+    out
+);
+  input in;
+  output out;
+  assign out = !in;
+  sub sub ();
+  initial $display("In '%m'");
+endmodule
+
+module b (
+    in,
+    out
+);
+  input in;
+  output out;
+  assign out = in;
+  sub sub ();
+  initial $display("In '%m'");
+endmodule
+
+module c (
+    uniq_in,
+    uniq_out
+);
+  input uniq_in;
+  output uniq_out;
+  assign uniq_out = !uniq_in;
+  sub sub ();
+  initial $display("In '%m'");
+endmodule
+
+module sub;
+  initial $display("In '%m'");
+endmodule
