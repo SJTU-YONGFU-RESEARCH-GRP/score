e0bde7a358c0c4f38a97ed83a3ef6eb310e86c3b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:13:40 +0800

    t_display_esc_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_display_esc_bad.v
@@ -0,0 +1,11 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  initial begin
+    $display("\x\y\z");  // Illegal escapes
+  end
+endmodule
