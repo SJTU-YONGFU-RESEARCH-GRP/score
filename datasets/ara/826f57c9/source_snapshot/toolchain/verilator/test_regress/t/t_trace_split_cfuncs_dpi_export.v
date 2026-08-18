3620e5824282ce87dcb82650eead79747fd8b6f0
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:58:47 +0800

    t_trace_split_cfuncs_dpi_export.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_split_cfuncs_dpi_export.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Varun Koyyalagunta
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  function automatic void func();
+  endfunction
+  export "DPI-C" function func;
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars();
+  end
+
+endmodule
