a96f774e4160cc185ed197c4434c90a020b86a85
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:18:06 +0800

    t_flag_language.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_flag_language.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2008 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  // See also t_preproc_kwd.v
+
+  integer bit; initial bit = 1;
+
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
