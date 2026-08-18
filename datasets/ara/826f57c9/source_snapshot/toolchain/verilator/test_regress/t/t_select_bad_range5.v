86899a792235498e01bb574886c0a2b307a41919
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:49:22 +0800

    t_select_bad_range5.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_select_bad_range5.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (  /*AUTOARG*/
+    // Inputs
+    clk,
+    unk,
+    mi
+);
+
+  input clk;
+  input unk;
+  output mi;
+
+  assign mi = unk[3:2];
+endmodule
