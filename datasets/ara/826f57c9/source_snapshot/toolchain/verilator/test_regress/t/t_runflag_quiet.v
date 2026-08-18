175103637ef324b804c9447cd14a6dd0c5a7a9ce
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:48:30 +0800

    t_runflag_quiet.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_runflag_quiet.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+timeunit 1us; timeprecision 1ns;
+
+module t;
+  initial begin
+    #10;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
