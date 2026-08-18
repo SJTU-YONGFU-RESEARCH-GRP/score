6c4c4d3fe70d7a64ea79776055a62b5b20ed8444
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:27:17 +0800

    t_inst_public.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_inst_public.v
@@ -0,0 +1,31 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  Pub pub ();
+
+  localparam ZERO = 0;
+  if (ZERO) Dead dead ();
+
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
+
+module Pub;
+
+  // verilator public_module
+
+  // no signals here
+
+endmodule
+
+module Dead;
+  // verilator public_module
+endmodule
