0344086ae059d40871693c9735ad59c4d563c053
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:38:07 +0800

    t_mod_uselib.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_mod_uselib.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+//   // `uselib {dir=<lib_diry> | file=<lib_file> | libext=<file_ext> | lib=<lib_name>
+`uselib libext=.v
+   s s ();
+endmodule
+
+module s;
+  initial begin
+     $write("*-* All Finished *-*\n");
+     $finish;
+   end
+endmodule
