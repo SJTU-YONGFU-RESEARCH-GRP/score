b3246fe024e785d7cca44245098bd59cc90493f7
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:02:03 +0800

    t_unpacked_concat_bad3.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_unpacked_concat_bad3.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Driss Hafdi
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  localparam logic [7:0] TOO_FEW[5] = '{0, 1, 2 ** 8 - 1};  // Bad
+
+  initial begin
+    $display("%p", TOO_FEW);
+    $stop;
+  end
+endmodule
