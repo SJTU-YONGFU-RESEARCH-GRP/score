36646054c8ed905c921596222c96c4fc6390fe07
yongfu <liyongfu.sg@gmail.com>
2026-08-18 22:08:26 +0800

    filelist.f

---
+++ datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/ethernet/filelist.f
@@ -0,0 +1,2 @@
+/home/yongfu/proj/score/datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/ethernet/grethaxi.vhd
+/home/yongfu/proj/score/datasets/riscv_vhdl/7f3e3a36/rtl_designs/peripherals/ethernet/types_common.vhd
