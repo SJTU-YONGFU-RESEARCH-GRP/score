2a95c76375d132c0417fa1cb608c4cefcef214a9
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:18:38 +0800

    t_flag_parameter_pkg.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_flag_parameter_pkg.v
@@ -0,0 +1,23 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2021 Adrien Le Masle
+// SPDX-License-Identifier: CC0-1.0
+
+package pack_a;
+  parameter PARAM_A = 0;
+endpackage : pack_a
+
+//module t;
+module t;
+
+  parameter PARAM_A = 0;
+
+  initial begin
+    $display(PARAM_A);
+    if (PARAM_A != 1) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
