e43f5b6be84bcc8735f3a5a4b4732c51a85b6be8
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:06:13 +0800

    t_class_mod_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_mod_bad.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// verilator lint_off MULTITOP
+
+module M;
+  class Cls;
+    function string name;
+      return $sformatf("m %m");
+    endfunction
+  endclass
+endmodule
+
+module t;
+  string s;
+
+  initial begin
+    M::Cls p;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
