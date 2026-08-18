b25fb38aa9dd276d3d56d0e82dbb756a97cb32d5
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:03:56 +0800

    t_var_rsvd_port.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_var_rsvd_port.v
@@ -0,0 +1,22 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2005 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (  /*AUTOARG*/
+    // Inputs
+    bool
+);
+
+  input bool;  // BAD
+
+  reg vector;  // OK, as not public
+  reg switch  /*verilator public*/;  // Bad
+  reg free  /*verilator public*/;  // OK, not actually a keyword
+
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
