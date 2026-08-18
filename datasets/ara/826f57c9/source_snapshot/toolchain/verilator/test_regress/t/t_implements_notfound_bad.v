dc9f35592b6f615f8f7c63ac2b2d4f36ae2717c5
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:45 +0800

    t_implements_notfound_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_implements_notfound_bad.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class ClsI implements Inotfound;
+endclass
+
+module t;
+  ClsI ci;
+endmodule
