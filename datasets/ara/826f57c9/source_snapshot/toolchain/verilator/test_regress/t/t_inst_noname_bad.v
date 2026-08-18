b8f915e2fc313521e735046d8f575db1751a2b87
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:27:03 +0800

    t_inst_noname_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_inst_noname_bad.v
@@ -0,0 +1,13 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Anthony Donlon
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  m();
+  m();
+endmodule
+
+module m;
+endmodule
