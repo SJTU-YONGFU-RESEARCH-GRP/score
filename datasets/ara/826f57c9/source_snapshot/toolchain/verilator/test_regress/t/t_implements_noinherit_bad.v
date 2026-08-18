e24676248c5578f50da5ff254afbb3e2b9e220af
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:41 +0800

    t_implements_noinherit_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_implements_noinherit_bad.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface class Icls;
+  localparam IP = 1;
+  typedef int i_t;
+endclass
+
+class Cls implements Icls;
+  function void f;
+    $display(IP); // Bad
+  endfunction
+endclass
+
+module t;
+  Cls c;
+endmodule
