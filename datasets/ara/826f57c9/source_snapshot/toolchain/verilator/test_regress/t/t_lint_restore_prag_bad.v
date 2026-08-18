ad4ebfe6038ff770baac53a0e8c98b9d442bc1c2
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:33:42 +0800

    t_lint_restore_prag_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_restore_prag_bad.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2007 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  // No matching save
+  // verilator lint_restore
+
+endmodule
