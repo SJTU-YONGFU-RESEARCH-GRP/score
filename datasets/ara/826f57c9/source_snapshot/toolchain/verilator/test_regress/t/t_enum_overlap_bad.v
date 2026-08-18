1826c39b3a408a56f3d465e212b0a166120e1ace
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:16:23 +0800

    t_enum_overlap_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_enum_overlap_bad.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2009 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  enum {
+    e0,
+    e1,
+    e2,
+    e1b = 1
+  } BAD1;
+
+  initial begin
+    $stop;
+  end
+
+endmodule
