fa8e495b6b739c849742a2c59b2108e947bfe7d9
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:32:54 +0800

    t_lint_latch_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_latch_bad.v
@@ -0,0 +1,30 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2010 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (/*AUTOARG*/
+  // Outputs
+  bl, cl, bc, cc,
+  // Inputs
+  a
+  );
+
+  input logic a;
+  output logic bl;
+  output logic cl;
+  always_latch begin
+    bl <= a;  // No warning
+    cl = a;
+  end
+
+  output logic bc;
+  output logic cc;
+  always_comb begin
+    bc <= a;  // Warning
+    cc = a;
+  end
+
+
+endmodule
