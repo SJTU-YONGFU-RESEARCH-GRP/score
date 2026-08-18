35966f3f2477af0c9d480e8428db808f839db6a7
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:44:03 +0800

    t_preproc_debugi.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_preproc_debugi.v
@@ -0,0 +1,31 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2009 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// verilog_format: off
+`define FOO
+`define BAR(aa,bb) aa bb
+`FOO
+`BAR(aa,bb)
+
+`ifdef FOO
+`else
+`endif
+`ifndef FOO
+`elsif FOO
+`endif
+
+`define STRINGIFY(x) `"x`"
+`define CONCAT(a, b) a``b
+`STRINGIFY(x)
+`CONCAT(x,y)
+
+`undef FOO
+
+`undefineall
+
+`ifdef NEVER
+`error "should not get"
+`endif
