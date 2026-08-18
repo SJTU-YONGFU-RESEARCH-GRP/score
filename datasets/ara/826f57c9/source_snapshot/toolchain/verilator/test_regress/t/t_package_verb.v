6fc5fc4387edc69d5ae6e372239ded88725ae003
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:40:51 +0800

    t_package_verb.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_package_verb.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2013 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// bug474
+package verb_pkg;
+  typedef enum int {
+    VERB_I,
+    VERB_W
+  } Verb_t;
+  Verb_t verb = VERB_I;
+  string message = " ";
+endpackage
+
+module t;
+  import verb_pkg::*;
+
+  string message = "*x*";
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
