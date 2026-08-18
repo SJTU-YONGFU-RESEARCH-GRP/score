001324e28dec5e3f38995118546ce4e5cd308759
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:48:03 +0800

    t_randstate_obj.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_randstate_obj.v
@@ -0,0 +1,39 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls;
+  rand int length;
+endclass
+
+module t;
+
+  int rand_result, v1, v2;
+  string s;
+
+  initial begin
+    Cls c;
+    c = new;
+
+    s = c.get_randstate();
+
+    rand_result = c.randomize();
+    if (rand_result != 1) $stop;
+    v1 = c.length;
+
+    c.set_randstate(s);
+
+    rand_result = c.randomize();
+    if (rand_result != 1) $stop;
+    v2 = c.length;
+
+`ifdef VERILATOR  // About half of the other simulators fail at this
+    if (v1 != v2) $stop;
+`endif
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
