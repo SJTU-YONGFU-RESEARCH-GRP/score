01580d7bdaff741b82eb2080c36f0148f90f4968
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:49:23 +0800

    t_select_bad_range6.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_select_bad_range6.v
@@ -0,0 +1,22 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  logic [11:0] i;
+  logic [30:0] o;
+
+  assign o = i[31:1];
+
+  always @(posedge clk) begin
+    i = 12'h123;
+  end
+  always @(negedge clk) begin
+    $write("Bad select %x\n", o);
+  end
+endmodule
