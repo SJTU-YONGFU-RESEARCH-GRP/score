070f2ab27d6b9e95450025485de58151fe14d09f
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:05:36 +0800

    t_class_extern_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_extern_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class Base1;
+  extern task nodef();
+  extern task nodef();  // <--- Error: duplicate
+endclass
+
+task Base1::noproto();  // <--- Error: Missing prototype
+endtask
+
+module t;
+endmodule
