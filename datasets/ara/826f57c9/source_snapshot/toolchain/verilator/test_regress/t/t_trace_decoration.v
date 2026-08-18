55a7d33aff51a837903ce9fab25947b513e2c18c
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:57:06 +0800

    t_trace_decoration.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_decoration.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2014 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  integer a_very_long_name_which_we_will_hash_eventually = 0;
+
+  always @(posedge clk) begin
+    a_very_long_name_which_we_will_hash_eventually <= a_very_long_name_which_we_will_hash_eventually + 1;
+    if (a_very_long_name_which_we_will_hash_eventually == 5) begin
+      fin();
+    end
+  end
+
+  task fin;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  endtask
+
+endmodule
