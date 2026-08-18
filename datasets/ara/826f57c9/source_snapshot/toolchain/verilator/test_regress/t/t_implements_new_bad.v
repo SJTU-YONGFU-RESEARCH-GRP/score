8e18626e4eeda9fd6fe318e035684408eba0ced3
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:40 +0800

    t_implements_new_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_implements_new_bad.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface class Icls;
+endclass
+
+module t;
+  Icls c;
+  initial begin
+    c = new;  // Bad
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
