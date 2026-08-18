ed976ab5596693045fde49c82ee37ee36288f016
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:11:14 +0800

    t_cover_const_compare.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_cover_const_compare.v
@@ -0,0 +1,30 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  integer cyc;
+  initial cyc = 1;
+
+  wire a = cyc[0];
+  wire b = cyc[0];
+
+  always @(posedge clk) begin
+    cyc <= cyc + 1;
+    // Before this was optimized, with --coverage-line
+    // error: self-comparison always evaluates to true [-Werror=tautological-compare]
+    //        if (((1U & vlSelf->t__DOT__cyc) == (1U & vlSelf->t__DOT__cyc)))
+    if (a != cyc[0]) $stop;  // Becomes cyc == cyc after substitution
+    if (b != cyc[0]) $stop;
+    if (cyc == 10) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+
+endmodule
