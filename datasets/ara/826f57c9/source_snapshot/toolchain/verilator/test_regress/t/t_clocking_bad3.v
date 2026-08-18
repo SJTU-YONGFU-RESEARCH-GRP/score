c18cbd5dfc0339707c1ddd84f3b9067fef3cec43
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:08:39 +0800

    t_clocking_bad3.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_clocking_bad3.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  clocking cb @(posedge clk);
+    input in;
+    output out;
+  endclocking
+
+  clocking cb @(posedge clk);
+  endclocking
+endmodule
