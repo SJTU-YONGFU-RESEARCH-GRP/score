1236f77a5ff352eaa1c74d00cef233c233f79abb
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:06:59 +0800

    t_class_param_lvalue.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_param_lvalue.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+class Foo;
+endclass
+
+class Bar #(
+    type BASE = Foo
+) extends BASE;
+  task body();
+    int v = 0;
+    v = 1;
+  endtask
+endclass
