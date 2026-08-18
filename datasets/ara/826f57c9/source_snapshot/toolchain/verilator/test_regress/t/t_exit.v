a5950cbfaa4d0176df1c6cae8b2733e2c5164aec
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:17:03 +0800

    t_exit.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_exit.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+program t;
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $exit;  // Must be in program block
+  end
+endprogram
