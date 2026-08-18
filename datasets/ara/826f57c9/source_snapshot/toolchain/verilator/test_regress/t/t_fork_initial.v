df058f735be4294f2e2346417b404794e6f72e43
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:20:57 +0800

    t_fork_initial.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_fork_initial.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  initial
+  fork
+    reg i;
+    i = 1'b1;
+    begin
+      #1;
+      if (i != 1'b1) $stop;
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  join
+endmodule
