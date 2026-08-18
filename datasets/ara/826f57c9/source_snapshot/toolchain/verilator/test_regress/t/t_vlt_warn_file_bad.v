489135365459ebd7d328743e3af3279ed53ddfac
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:05:18 +0800

    t_vlt_warn_file_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_vlt_warn_file_bad.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+`include "t_vlt_warn_file_bad_b.vh"
+
+module t;
+  sub sub ();
+  int warn_t = 64'h1;
+endmodule
