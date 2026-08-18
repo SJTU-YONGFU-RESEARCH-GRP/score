e723b4b1746a0638c0917b7bc20336db8ef441c5
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:23:16 +0800

    t_fuzz_genintf_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_fuzz_genintf_bad.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+//bug1588
+interface intf;
+  logic a;
+  modport source(output a);
+endinterface
+
+module m1 (
+    input logic value,
+    intf.source b
+);
+endmodule
+
+module t;
+  intf ifs ();
+
+  m1 m0 (
+      j.e(0),
+      .b(ifs)
+  );
+
+  genvar j;
+endmodule
