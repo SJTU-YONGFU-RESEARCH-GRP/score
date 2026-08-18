398c4c41dcebc0b6545f91f1876535615daf57fd
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:29:19 +0800

    t_interface_missing_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_interface_missing_bad.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Missing interface test
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2015 Todd Strader
+// SPDX-License-Identifier: CC0-1.0
+
+// Interface intentionally not defined
+//interface foo_intf;
+//   logic a;
+//endinterface
+
+module foo_mod (
+    foo_intf foo
+);
+endmodule
+
+module t;
+
+  foo_intf the_foo ();
+
+  foo_mod foo_mod (.foo(the_foo));
+
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
