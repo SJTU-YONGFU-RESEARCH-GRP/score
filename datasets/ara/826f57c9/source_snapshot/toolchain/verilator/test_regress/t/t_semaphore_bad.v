49cca63e67d230705f860e66fde65bd07e3ac706
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:49:55 +0800

    t_semaphore_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_semaphore_bad.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  semaphore s;
+
+  initial begin
+    s = new(4);
+    if (s.bad_method() != 0) $stop;
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
