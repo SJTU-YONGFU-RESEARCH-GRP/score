7175f2abcd38b69c00851fe91a1d70fc216c1936
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:59:24 +0800

    t_tri_cond_eqcase_with_1.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_tri_cond_eqcase_with_1.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+  wire a;
+  assign a = 1 === (clk ? 1 : 1'bz);
+
+  always begin
+    if (!a) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+endmodule
