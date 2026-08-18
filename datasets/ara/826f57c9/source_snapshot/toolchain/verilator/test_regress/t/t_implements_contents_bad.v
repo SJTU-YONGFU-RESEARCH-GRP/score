3ca525eae992dbfee6bf0cdc9b96f455340db0f6
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:35 +0800

    t_implements_contents_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_implements_contents_bad.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface class Icls;
+  int badi;
+  task badtask;
+  endtask
+endclass
+
+module t;
+endmodule
