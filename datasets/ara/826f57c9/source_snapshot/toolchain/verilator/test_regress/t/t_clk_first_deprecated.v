034aa86e72dffb125a071f801391de5a80d7cfd8
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:08:21 +0800

    t_clk_first_deprecated.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_clk_first_deprecated.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2003 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (  /*AUTOARG*/
+    // Inputs
+    clk
+);
+
+  input clk  /*verilator sc_clock*/;
+
+endmodule
