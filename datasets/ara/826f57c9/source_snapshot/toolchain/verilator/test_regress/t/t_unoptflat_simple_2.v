23af4918a35fc1c15adef2e4a816f3fac1a80474
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:01:49 +0800

    t_unoptflat_simple_2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_unoptflat_simple_2.v
@@ -0,0 +1,34 @@
+// DESCRIPTION: Verilator: Simple test of unoptflat
+//
+// Simple demonstration of an UNOPTFLAT combinatorial loop, using 3 bits.
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2013 Jeremy Bennett
+// SPDX-License-Identifier: CC0-1.0
+
+module t (  /*AUTOARG*/
+    // Inputs
+    clk
+);
+  input clk;
+
+  /* verilator lint_off MULTIDRIVEN */
+  wire [2:0] x;
+  /* verilator lint_on MULTIDRIVEN */
+
+  assign x[1:0] = {x[0], clk};
+  assign x[2:1] = x[1:0];
+
+  always @(posedge clk or negedge clk) begin
+
+`ifdef TEST_VERBOSE
+    $write("x = %x\n", x);
+`endif
+
+    if (x[2] != 0) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+
+endmodule  // t
