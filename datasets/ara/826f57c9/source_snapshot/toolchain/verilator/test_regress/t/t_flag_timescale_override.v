6985445f10b88252d3d4db5220bf18755a5aa45a
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:19:05 +0800

    t_flag_timescale_override.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_flag_timescale_override.v
@@ -0,0 +1,23 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under The Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+`timescale 1s / 1s
+
+module t;
+  sub sub ();
+  initial begin
+    $printtimescale;
+    sub.pts();
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
+
+module sub;
+  task pts;
+    $printtimescale;
+  endtask
+endmodule
