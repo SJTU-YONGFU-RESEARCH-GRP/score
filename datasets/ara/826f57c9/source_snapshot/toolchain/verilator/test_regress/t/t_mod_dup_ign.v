6082eb596a037ec5638fd3c5ee965d6413b1238c
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:37:37 +0800

    t_mod_dup_ign.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_mod_dup_ign.v
@@ -0,0 +1,24 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2010 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  sub sub ();
+endmodule
+
+module sub;
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
+
+// verilator lint_off MODDUP
+module sub;
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
