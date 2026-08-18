99770f0ce508ad242328b6aa313478ef43e711b7
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:08:02 +0800

    t_class_virtual_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_virtual_bad.v
@@ -0,0 +1,14 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+virtual class VBase;
+endclass
+
+module t;
+  initial begin
+    automatic VBase b = new;  // Error
+  end
+endmodule
