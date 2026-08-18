6f575d29a893d48317aac2f2d252f99887637ba8
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:06:04 +0800

    t_class_membersel_int.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_membersel_int.v
@@ -0,0 +1,24 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls;
+  int t;
+endclass
+
+module Sub;
+  Cls c;
+  initial begin
+    int i;
+    c = new;
+    i = c.t;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
+
+module t;
+  Sub foo ();
+endmodule
