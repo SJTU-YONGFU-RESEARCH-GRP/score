1790cf266346d06366d927e42ba35f5552601e76
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:04:39 +0800

    t_castdyn_unsup_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_castdyn_unsup_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  string q[$];
+  int aarray[string];
+
+  initial begin
+    $cast(q, aarray);
+  end
+
+endmodule
