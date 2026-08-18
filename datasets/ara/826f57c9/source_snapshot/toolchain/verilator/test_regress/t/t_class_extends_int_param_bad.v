5fa42afe9827acf560663bec0a68eb136a8ce934
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:05:21 +0800

    t_class_extends_int_param_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_extends_int_param_bad.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  class Bar #(
+      type T = int
+  ) extends T;
+  endclass
+
+  initial begin
+    Bar #() bar;
+    $stop;
+  end
+endmodule
