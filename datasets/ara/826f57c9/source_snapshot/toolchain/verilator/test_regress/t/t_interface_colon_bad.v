c4e981d788b44e49ce59ef08c2b6ff6ee3f150d0
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:28:06 +0800

    t_interface_colon_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_interface_colon_bad.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface iface;
+  function static func;
+  endfunction
+endinterface
+
+module t;
+  initial begin
+    iface::func();  // BAD
+    $stop;
+  end
+endmodule
