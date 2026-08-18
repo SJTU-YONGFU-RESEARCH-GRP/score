368de6be19fdf405ca80bb1b6ebbbf65e2727765
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:20:56 +0800

    t_fork_func_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_fork_func_bad.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  function int f;
+    fork
+      return 0;  // Illegal 9.3.2
+    join_none
+  endfunction
+
+  int i;
+
+  initial begin
+    i = f();
+  end
+
+endmodule
