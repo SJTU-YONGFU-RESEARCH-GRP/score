fc33773b3d83b82096b6ce549b2b1c8245efe735
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:36:01 +0800

    t_math_real_public.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_math_real_public.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2018 Alex Solomatnikov
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  sub #(.REAL_PARAM(2.0)) sub ();
+endmodule
+
+module sub ();
+  parameter REAL_PARAM = 0.0;  // Magic name grepped for in .py file
+
+  initial begin
+    $display("REAL_PARAM=%g", REAL_PARAM);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
