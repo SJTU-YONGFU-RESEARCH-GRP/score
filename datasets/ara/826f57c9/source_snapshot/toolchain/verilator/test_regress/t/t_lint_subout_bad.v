e375245ebaf84268a7cb0552b728bdcfd8d367f1
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:33:51 +0800

    t_lint_subout_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_subout_bad.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// verilator lint_off UNDRIVEN
+
+module t;
+  wire sig;
+  sub sub0 (.out(33'b0));
+  sub sub1 (.out({32'b0, sig}));
+  sub sub2 (.out({32'b1, sig}));
+endmodule
+
+module sub (
+    output reg [32 : 0] out
+);
+endmodule
