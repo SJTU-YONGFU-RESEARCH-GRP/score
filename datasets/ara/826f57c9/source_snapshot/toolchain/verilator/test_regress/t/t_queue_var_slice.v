f2b2bc6792b03d406a9ed715648da286fcd51ec4
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:46:19 +0800

    t_queue_var_slice.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_queue_var_slice.v
@@ -0,0 +1,23 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  integer i = 0;
+  integer q[$] = {0, 1};
+
+  always @(posedge clk) begin
+    $display("%p", q[i:i+1]);
+    q.push_back(i + 2);
+    i++;
+    if (i >= 3) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+endmodule
