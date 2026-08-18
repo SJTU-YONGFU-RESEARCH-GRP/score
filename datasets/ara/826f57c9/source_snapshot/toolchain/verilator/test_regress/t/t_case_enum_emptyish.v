f2f3133470707014d541dd74649619eb21c69353
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:03:50 +0800

    t_case_enum_emptyish.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_case_enum_emptyish.v
@@ -0,0 +1,29 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  enum logic [2:0] {
+    e0,
+    e1,
+    e2,
+    e3
+  } EN;
+
+  initial begin
+
+    unique case (EN)
+      e0: ;
+      e1: ;
+      e2: ;
+      e3: ;
+    endcase
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
