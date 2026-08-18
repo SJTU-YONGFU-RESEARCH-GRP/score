6734742c39c0608872ab9163f37c59bd52d424fc
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:05:17 +0800

    t_class_extends_colon.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_extends_colon.v
@@ -0,0 +1,37 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface class Icempty;
+endclass : Icempty
+
+package Pkg;
+  class Icls1 #(
+      parameter PARAM = 12
+  );
+    localparam LP1 = 1;
+    function int getParam();
+      return PARAM;
+    endfunction
+  endclass
+
+endpackage
+
+class Cls12 extends Pkg::Icls1;
+endclass
+
+module t;
+
+  Cls12 cp12;
+
+  initial begin
+    cp12 = new;
+    if (cp12.getParam() != 12) $stop;
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
