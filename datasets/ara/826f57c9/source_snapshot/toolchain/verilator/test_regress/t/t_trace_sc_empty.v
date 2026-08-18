e386be76c2c0508beae94a21fbed6967c7060f3b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:58:40 +0800

    t_trace_sc_empty.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_sc_empty.v
@@ -0,0 +1,13 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    output id0
+);
+
+  assign id0 = 0;
+
+endmodule
