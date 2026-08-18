5ac3718e76bd755b1bd87d6d01079dc106e8193e
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:30:01 +0800

    t_interface_paren_missing_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_interface_paren_missing_bad.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Goekce Aydos
+// SPDX-License-Identifier: CC0-1.0
+
+// Interface instantiation without parenthesis
+
+interface intf;
+endinterface
+
+module t;
+  intf intf_i;
+  initial $finish;
+endmodule
