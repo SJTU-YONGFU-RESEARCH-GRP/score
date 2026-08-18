efa90269b727c4033fffc14786d72341846ebf1f
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:32:29 +0800

    t_lint_implicit_port.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_implicit_port.v
@@ -0,0 +1,40 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2008 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  logic oe;
+
+  read r (
+      .clk(clk),
+      .data(((oe == 1'd001) && implicit_write))
+  );
+  sets s (
+      .clk(clk),
+      .enable(implicit_write)
+  );
+  read u (
+      .clk(clk),
+      .data(~implicit_also)
+  );
+
+endmodule
+
+module sets (
+    input clk,
+    output enable
+);
+  assign enable = 1'b0;
+endmodule
+
+module read (
+    input clk,
+    input data
+);
+
+endmodule
