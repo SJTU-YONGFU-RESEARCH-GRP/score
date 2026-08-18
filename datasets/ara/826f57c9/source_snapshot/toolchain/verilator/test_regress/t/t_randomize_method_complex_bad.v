caaa6e9e142f75814847ffdd0c4a02a9498b07f3
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:47:03 +0800

    t_randomize_method_complex_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_randomize_method_complex_bad.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls;
+  Cls f;
+  rand int r;
+endclass
+module t;
+  Cls x = new;
+  int i;
+  // verilog_format: off
+  initial $display(
+    x.f.randomize(),
+    x.f.randomize() with { r < 5; },
+    i.randomize() with { v < 5; });
+endmodule
