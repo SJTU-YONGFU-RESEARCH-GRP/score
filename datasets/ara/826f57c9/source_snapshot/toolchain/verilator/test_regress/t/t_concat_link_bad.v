76797e603c2c105d94cd9c5c2f74050a1930ae01
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:09:18 +0800

    t_concat_link_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_concat_link_bad.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain.
+// SPDX-FileCopyrightText: 2019
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  typedef logic [3:0] foo_t;
+
+  foo_t foo_s;
+
+  assign bar_s = {foo_s, foo_s}.f1;
+
+endmodule
