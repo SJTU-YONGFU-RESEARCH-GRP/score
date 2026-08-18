4b74ce056bfb901deeaa614eb8ed7c266a804afa
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:51:18 +0800

    t_strength_2_uneq_assign.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_strength_2_uneq_assign.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  wire [5:0] a;
+  assign (weak0, strong1) a = clk ? 'z : '0;
+  assign (strong0, pull1) a = 6'b110001;
+  initial begin
+    if (a === 6'b110001) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+
+endmodule
