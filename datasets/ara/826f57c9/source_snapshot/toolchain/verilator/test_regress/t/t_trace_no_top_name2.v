eb3232c214bc8dd85d04d40b0af28c956170c16e
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:58:06 +0800

    t_trace_no_top_name2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_no_top_name2.v
@@ -0,0 +1,36 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+package foo_pkg;
+  function int foo_func;
+    input int b;
+    int b_current;
+    return 0;
+  endfunction
+endpackage
+
+module sub;
+  int a = 1212;
+endmodule
+
+module t (
+    input clk
+);
+
+  int cyc;
+
+  import foo_pkg::*;
+
+  sub sub ();
+
+  always @(posedge clk) begin
+    cyc <= cyc + 1;
+    if (cyc == 10) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+endmodule
