a12d73e5734bbe1125811e0261a7f41364249eec
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:13:44 +0800

    t_display_impure.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_display_impure.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+function integer f;
+  static integer i = 0;
+  return ++i;
+endfunction
+
+module t;
+  initial begin
+    $display("%d", f());
+    $display("%d", f());
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
