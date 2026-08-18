92ba1aebf6252d7ff5e663e82f85765b4333129d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:18:05 +0800

    t_flag_j_hier.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_flag_j_hier.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain
+// SPDX-FileCopyrightText: 2024 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  logic a;
+  s u_s (.a(a));
+endmodule
+
+module s (
+    output logic a
+);
+  /*verilator hier_block*/
+endmodule
