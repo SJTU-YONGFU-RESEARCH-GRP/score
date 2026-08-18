f7ea9cd9a20a6e49677e505d3ad10608a2db756d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:32 +0800

    t_implements_collision.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_implements_collision.v
@@ -0,0 +1,37 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface class Icls1;
+  pure virtual function int icfboth;
+endclass
+
+interface class Icls2;
+  pure virtual function int icfboth;
+endclass
+
+interface class IclsBoth extends Icls1, Icls2;
+  pure virtual function int icfboth;
+endclass
+
+class Cls implements IclsBoth;
+  virtual function int icfboth;
+    return 3;
+  endfunction
+endclass
+
+module t;
+
+  Cls c;
+
+  initial begin
+    c = new;
+    if (c.icfboth() != 3) $stop;
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
