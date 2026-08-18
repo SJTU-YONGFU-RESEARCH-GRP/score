d7ed79b879ab4e0a8bcf993ddd8a07ffbd3106b5
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:24:57 +0800

    t_hier_block_libmod.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_hier_block_libmod.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Yutetsu TAKATSUKASA
+// SPDX-License-Identifier: Unlicense
+
+module t;
+  t_flag_relinc_sub i_t_flag_relinc_sub();
+endmodule
+
+`ifdef VERILATOR
+`verilator_config
+hier_block -module "t_flag_relinc_sub"
+`verilog
+`endif
