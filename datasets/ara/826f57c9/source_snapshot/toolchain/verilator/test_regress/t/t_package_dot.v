0e22a16d748a47a536d8d89f00fddea946ff693b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:40:32 +0800

    t_package_dot.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_package_dot.v
@@ -0,0 +1,26 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2015 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+package pkg;
+  typedef struct packed {
+    logic [3:0] msk;
+    logic [3:0] dat;
+  } STR_t;
+endpackage
+;
+
+package csr_pkg;
+  typedef pkg::STR_t reg_t;
+  localparam reg_t REG_RST = 8'h34;
+endpackage
+
+module t;
+  initial begin
+    if (csr_pkg::REG_RST.msk != 4'h3) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
