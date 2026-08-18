f8fecbca8074209f6cfc5ca52da566ac028c4c2f
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:30:16 +0800

    t_interface_typo_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_interface_typo_bad.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2016 Todd Strader
+// SPDX-License-Identifier: CC0-1.0
+
+//bug1097
+
+interface foo_intf;
+endinterface
+
+module submod (
+    foo_intf foo
+);
+
+endmodule
+
+module t;
+  // Intentional typo, compiler should point this out, or that fo_intf does
+  // not match foo_intf on the submod port map
+  fo_intf the_foo ();
+
+  submod submod_inst (.foo(the_foo));
+
+endmodule
