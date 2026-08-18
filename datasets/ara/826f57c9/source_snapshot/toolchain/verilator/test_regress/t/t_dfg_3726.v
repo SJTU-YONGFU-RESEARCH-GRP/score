eccf1994d5ef01616abea01cb7c1ee657b51f2ae
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:12:39 +0800

    t_dfg_3726.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_dfg_3726.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Geza Lore
+// SPDX-License-Identifier: CC0-1.0
+
+module t (  /*AUTOARG*/
+    // Outputs
+    x,
+    // Inputs
+    i
+);
+
+  input i;
+  output x;
+
+  assign x = (i ? 0 : 1) && 1;
+
+endmodule
