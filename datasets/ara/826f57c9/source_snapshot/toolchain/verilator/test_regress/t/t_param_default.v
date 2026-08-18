3a80ea650ccaa416e3bd0c43fbb2478d8a466176
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:41:14 +0800

    t_param_default.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_default.v
@@ -0,0 +1,22 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2003 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module m #(
+    parameter int Foo
+);
+endmodule
+
+module t;
+
+  m #(10) foo ();
+
+  initial begin
+    if (foo.Foo != 10) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
