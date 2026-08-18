670b48d71849c72e63a2070ddb3977363ca4361f
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:03:01 +0800

    t_assigndly_dynamic_notiming_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_assigndly_dynamic_notiming_bad.v
@@ -0,0 +1,23 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls;
+  task bar;
+    static int qux;
+    qux <= '1;
+    // Use qux to prevent V3Dead optimizations
+    $display("qux = %d\n", qux);
+  endtask
+endclass
+
+module t;
+  initial begin
+    Cls c;
+    c.bar();
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
