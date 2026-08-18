985efccc7348391c7cd6e217d272f89d108e86ae
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:52:16 +0800

    t_struct_type_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_struct_type_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2012 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  int i;
+
+  typedef struct packed {
+    int i;
+    i badi;  // Bad
+  } struct_t;
+
+endmodule
