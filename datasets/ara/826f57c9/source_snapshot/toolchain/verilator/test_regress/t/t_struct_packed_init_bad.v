bbf06431226a576e8a56cd275df59246989d01b5
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:52:04 +0800

    t_struct_packed_init_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_struct_packed_init_bad.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  parameter P = 4'h5;
+
+  struct packed {
+    bit [3:0] m_lo = P;  // Bad
+    bit [3:0] m_hi;
+  } s;
+
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
