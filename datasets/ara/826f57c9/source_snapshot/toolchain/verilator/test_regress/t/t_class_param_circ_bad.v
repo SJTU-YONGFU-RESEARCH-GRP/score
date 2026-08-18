315d0644777adc16fc570e2a40f3d1cfe0626f3f
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:06:47 +0800

    t_class_param_circ_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_param_circ_bad.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+typedef class ClsB;
+
+class ClsA #(
+    parameter PARAM = 12
+);
+  ClsB #(PARAM + 1) b;
+endclass
+
+class ClsB #(
+    parameter PARAM = 12
+);
+  ClsA #(PARAM + 1) a;
+endclass
+
+module t;
+
+  ClsA #(.PARAM(15)) c;  // Bad param name
+
+endmodule
