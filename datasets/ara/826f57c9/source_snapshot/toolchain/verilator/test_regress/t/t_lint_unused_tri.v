2225b524ca3667269073f498859deb2f348bd249
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:34:05 +0800

    t_lint_unused_tri.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_unused_tri.v
@@ -0,0 +1,30 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module Receiver (
+    in
+);
+  inout [31:0] in;
+  always @(in) $display(in);
+endmodule
+
+module Sender (
+    out
+);
+  inout [31:0] out;
+  assign out = 12;
+endmodule
+
+module t;
+  // ports of submodule recv
+  tri [31 : 0] recvIn;
+
+  // submodule recv
+  Receiver recv (.in(recvIn));
+
+  // submodule send
+  Sender send (.out(recvIn));
+endmodule
