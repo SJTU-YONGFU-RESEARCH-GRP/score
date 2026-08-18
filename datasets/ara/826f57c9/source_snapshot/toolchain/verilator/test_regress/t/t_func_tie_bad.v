0e570ad895b877a3ff6ff9388418ae4cebad62af
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:22:51 +0800

    t_func_tie_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_func_tie_bad.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2011 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  initial begin
+    // verilator lint_off IGNOREDRETURN
+    func(0, 1'b1);
+  end
+
+  function automatic int func(input int a, output bit b);
+    return 0;
+  endfunction
+
+endmodule
