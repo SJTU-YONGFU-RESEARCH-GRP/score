8539f235146ad74034e788c9ebae3f620790479a
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:17:18 +0800

    t_fallback_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_fallback_bad.v
@@ -0,0 +1,45 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+int f = 5;
+task tsk;
+endtask
+
+package pkg;
+endpackage
+
+module subm;
+endmodule
+
+module submo;
+  subm sub2 ();
+endmodule
+
+module t;
+  submo sub1 ();
+
+  class Base;
+  endclass
+  class Cls extends Base;
+    task calltsk;
+      super.tsk;
+      this.tsk;
+      super.f = 8;
+      this.f = 8;
+      sub1.sub2.tsk;
+      pkg::f = 8;
+      pkg::tsk();
+      sub1.sub2.f = 8;
+      sub1.sub2.f.f = 8;
+    endtask
+  endclass
+
+  Cls obj = new;
+  initial begin
+    obj.calltsk;
+    if (f != 5) $stop;
+  end
+endmodule
