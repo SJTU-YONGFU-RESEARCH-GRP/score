a7fa095adcdaf0fcc2b360e344bcfc48c69b06c6
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:58:56 +0800

    t_trace_two_b.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_two_b.v
@@ -0,0 +1,27 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  integer cyc;
+  initial cyc = 1;
+  integer c_trace_on;
+  real r;
+
+  sub sub ();
+
+  always @(posedge clk) begin
+    if (cyc != 0) begin
+      r <= r + 0.1;
+    end
+  end
+endmodule
+
+module sub;
+  integer inside_sub_a = 2;
+endmodule
