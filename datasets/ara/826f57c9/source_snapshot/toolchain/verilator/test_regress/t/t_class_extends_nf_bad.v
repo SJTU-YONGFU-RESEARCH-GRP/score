6896a66cc8938a3d77cf0ef6d7335c88a3808eaa
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:05:22 +0800

    t_class_extends_nf_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_extends_nf_bad.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+package Pkg;
+class otFound2;
+endclass
+endpackage
+
+class IsFound;
+endclass
+
+class Cls extends IsNotFound;  // BAD: not found
+endclass
+
+class Cls2 extends Pkg::NotFound2;  // BAD: not found
+  function new;
+    super.new();
+  endfunction
+endclass
+
+module t;
+endmodule
