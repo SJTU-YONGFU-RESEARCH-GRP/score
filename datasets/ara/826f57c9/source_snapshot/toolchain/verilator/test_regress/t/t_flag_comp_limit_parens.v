b9a35a0bd44e745af41e5f8e1931c7f6c3fa5cfb
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:17:28 +0800

    t_flag_comp_limit_parens.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_flag_comp_limit_parens.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2021 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (/*AUTOARG*/
+  // Inputs
+  r
+  );
+  input real r;
+
+  initial begin
+    $display("%g", $cos($cos($cos($cos($cos($cos($cos($cos(r + 0.1)))))))));
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
