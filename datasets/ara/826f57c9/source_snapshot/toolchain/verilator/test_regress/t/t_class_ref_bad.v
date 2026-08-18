4940842fe7369b761bf9851158892f5b6ea42e94
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:07:26 +0800

    t_class_ref_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_ref_bad.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class ClsRight;
+  string m_s;
+endclass
+
+module t;
+  string s;
+  initial begin
+    // verilator lint_off PKGNODECL
+    s = ClsRigh::m_s;  // Bad typo, issue #5475
+  end
+endmodule
