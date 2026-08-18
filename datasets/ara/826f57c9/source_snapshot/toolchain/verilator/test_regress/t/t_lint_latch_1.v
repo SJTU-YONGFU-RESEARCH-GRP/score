b4d6a44186d62258dd7b1d89c2bf1cc369fc4b7b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:32:45 +0800

    t_lint_latch_1.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_latch_1.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module for issue #1609
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Julien Margetts
+// SPDX-License-Identifier: Unlicense
+
+module t (  /*AUTOARG*/
+    a,
+    b,
+    o
+);
+  input a;
+  input b;
+  output reg o;
+
+  // verilator lint_off LATCH
+  always @(a or b) if (a) o <= b;
+
+endmodule
