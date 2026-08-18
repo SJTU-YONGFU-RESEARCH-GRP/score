c9dd4dc2d91b85222c16dfe285288d22b7edf75d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:10:53 +0800

    t_constraint_pure_missing_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_constraint_pure_missing_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+virtual class Base;
+   pure constraint raint;
+endclass
+
+class Cls extends Base;
+  // Bad: Missing 'constraint raint'
+endclass
+
+module t;
+endmodule
