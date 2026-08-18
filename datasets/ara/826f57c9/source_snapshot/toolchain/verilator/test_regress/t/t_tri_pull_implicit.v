ccea78d87a694a6a6a31444699c5121337b29ac8
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:59:54 +0800

    t_tri_pull_implicit.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_tri_pull_implicit.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  // verilator lint_off IMPLICIT
+  pulldown (pd);
+  pullup (pu);
+
+  initial begin
+    if (pd != 0) $stop;
+    if (pu != 1) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
