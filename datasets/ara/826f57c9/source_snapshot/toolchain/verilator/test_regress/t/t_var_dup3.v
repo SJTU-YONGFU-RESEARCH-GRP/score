97d56620c9f2b47ef918c4deee39256cd1e17367
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:03:08 +0800

    t_var_dup3.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_var_dup3.v
@@ -0,0 +1,33 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// Legal with Verilog 1995 style ports
+
+module t (  /*AUTOARG*/
+    // Outputs
+    ok_o_w,
+    ok_o_r,
+    ok_o_ra,
+    ok_or,
+    ok_ow,
+    ok_owa
+);
+
+  output ok_o_w;
+  wire ok_o_w;
+
+  output ok_o_r;
+  reg ok_o_r;
+
+  output [1:0] ok_o_ra;
+  reg [1:0] ok_o_ra;
+
+  output reg ok_or;
+
+  output wire ok_ow;
+
+  output wire [1:0] ok_owa;
+endmodule
