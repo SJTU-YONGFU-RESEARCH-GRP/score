6944912cea5f5a4c3c5c7becc5cab69d1fa07091
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:48:35 +0800

    t_sampled_expr_unsup.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_sampled_expr_unsup.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  int a = 0;
+
+  function int f(output int a);
+    a = 1;
+    return a;
+  endfunction
+
+  assert property (@(posedge clk) f(a) >= 0);
+endmodule
