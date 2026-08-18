fc5b3c04a58b24be5e1da44d0f090c812860843a
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:31:51 +0800

    t_lint_const_func_dpi_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_const_func_dpi_bad.v
@@ -0,0 +1,10 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2021 Donald Owen
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  import "DPI-C" function int dpiFunc();
+  localparam PARAM = dpiFunc();
+endmodule
