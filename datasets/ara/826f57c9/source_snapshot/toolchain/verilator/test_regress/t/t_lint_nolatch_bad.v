5cfb1063d301cac7bbee903a8bb1c35ab18b19de
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:33:14 +0800

    t_lint_nolatch_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_nolatch_bad.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module for issue #1609
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Julien Margetts
+// SPDX-License-Identifier: Unlicense
+
+module t (/*AUTOARG*/ a, b, o);
+  input  a;
+  input  b;
+  output reg o;
+
+  always_latch
+    if (a)
+     o = b;
+    else
+     o = ~b;
+
+endmodule
