ca32dc1ffff2d74c7568a19be64ab190ddf2d50d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:11:45 +0800

    t_cover_unused_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_cover_unused_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  logic unu3 = 0;
+
+  logic isusd = 0;
+  cover property (@(posedge clk) isusd == 0);
+
+endmodule
