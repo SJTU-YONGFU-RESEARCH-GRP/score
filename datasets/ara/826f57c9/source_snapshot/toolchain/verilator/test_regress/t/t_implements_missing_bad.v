66156546b79211e385601c13acaee54968c4bd49
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:36 +0800

    t_implements_missing_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_implements_missing_bad.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface class Icls1;
+  pure virtual function int icf1;
+  pure virtual function int icf2;
+endclass
+
+class Cls implements Icls1;
+  virtual function int icf1;
+    return 1;
+  endfunction
+  // Bad missing icf2
+endclass
+
+module t;
+  Cls c;
+endmodule
