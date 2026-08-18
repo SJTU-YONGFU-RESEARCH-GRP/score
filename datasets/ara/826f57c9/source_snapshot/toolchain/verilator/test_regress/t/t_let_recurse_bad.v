f70469959cd2a794a0943098efcd3f12c81ce831
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:31:14 +0800

    t_let_recurse_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_let_recurse_bad.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  let RECURSE(a) = (a == 1) ? 1 : RECURSE(a - 1);  // BAD no recursion per IEEE 1800-2023 11.12
+
+  initial begin
+    if (RECURSE(1) != 1) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
