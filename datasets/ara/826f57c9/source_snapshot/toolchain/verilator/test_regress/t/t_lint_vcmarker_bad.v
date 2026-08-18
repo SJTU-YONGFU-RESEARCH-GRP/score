b205fa0546a651e48b894006970ccfe68db1f5e6
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:34:10 +0800

    t_lint_vcmarker_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_lint_vcmarker_bad.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+<<<<<<< HEAD  // Intentional test: This conflict marker should be here
+  initial $display("Hello");
+=======   // Intentional test: This conflict marker should be here
+  initial $display("Goodbye");
+>>>>>>> MERGE  // Intentional test: This conflict marker should be here
+
+endmodule
