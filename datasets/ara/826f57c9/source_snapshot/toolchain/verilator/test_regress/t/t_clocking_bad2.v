34c1c697c73f951707ea0e0338c065f46e123c4e
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:08:37 +0800

    t_clocking_bad2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_clocking_bad2.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  logic in, out;
+  clocking cb @(posedge clk);
+    default input #1 output #1step;  // Now allowed
+    default input #2 output #2;
+    output #1step out;  // Now allowed
+    output out;
+  endclocking
+endmodule
