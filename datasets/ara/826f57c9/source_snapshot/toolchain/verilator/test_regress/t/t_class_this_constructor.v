3da199a5e0aae27356a4ee425e2ef1265924647e
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:07:52 +0800

    t_class_this_constructor.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_this_constructor.v
@@ -0,0 +1,26 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls;
+  bit x = 1'b0;
+  function new;
+    Cls c;
+    if (c == this) begin
+      x = 1'b1;
+    end
+  endfunction
+endclass
+
+module t;
+  Cls c;
+  initial begin
+    c = new;
+    if (c.x) $stop;
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
