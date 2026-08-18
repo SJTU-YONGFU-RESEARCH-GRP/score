c9f7af899f5ff562af4c8cf51d7638200a4bfec1
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:01:28 +0800

    t_array_method_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_array_method_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  initial begin
+    int q[5];
+
+    q.mex;
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
