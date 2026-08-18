fe7f8eeb75c47f67156425aedc2ebe0137619696
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:13:32 +0800

    t_display_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_display_bad.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2003 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  reg [40:0] disp;
+  initial disp = 41'ha_bbbb_cccc;
+  initial begin
+    // Display formatting
+    $display("%x");  // Too few
+    $display("%x", disp, disp);  // Too many
+    $display("%q");  // Bad escape
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
