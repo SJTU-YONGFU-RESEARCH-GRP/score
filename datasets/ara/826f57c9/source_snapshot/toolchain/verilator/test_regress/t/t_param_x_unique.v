832c2fdcf42c81d67713f1120d59ccfde587f466
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:42:30 +0800

    t_param_x_unique.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_x_unique.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module sub #(
+    parameter P = 1'bx
+);
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
+
+module t;
+  sub sub ();
+endmodule
