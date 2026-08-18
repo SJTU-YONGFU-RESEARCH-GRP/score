b3230fc301a6d3f3a6c1000843656eaeb500b5ae
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:06:01 +0800

    t_class_member_bad2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_member_bad2.v
@@ -0,0 +1,23 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class ClsDup;
+  int vardup;
+  int vardup;
+  task memdup;
+  endtask
+  task memdup;
+  endtask
+
+  function void funcdup;
+  endfunction
+  function void funcdup;
+  endfunction
+
+endclass
+
+module t;
+endmodule
