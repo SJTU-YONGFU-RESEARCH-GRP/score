59cfcac70baf354e9e6e71e6def87d961177c663
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:49:26 +0800

    t_select_bad_width0.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_select_bad_width0.v
@@ -0,0 +1,24 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  parameter int ZERO = 0;
+
+  initial begin
+    automatic bit [31:0] val = '1;
+    automatic int left = 4;
+
+    automatic int part = val[left+:ZERO];
+    $display(part);
+    part = val[left-:ZERO];
+    $display(part);
+
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
