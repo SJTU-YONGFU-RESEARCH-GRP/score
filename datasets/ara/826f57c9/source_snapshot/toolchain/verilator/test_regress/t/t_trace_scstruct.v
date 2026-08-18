d5915230a38fd0490996001d68f89ee8036d9c1c
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:58:45 +0800

    t_trace_scstruct.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_scstruct.v
@@ -0,0 +1,26 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2014 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// verilator lint_off UNUSED
+// verilator lint_off UNDRIVEN
+
+//bug858
+
+typedef struct packed {
+  logic m_1;
+  logic m_2;
+} struct_t;
+
+typedef struct packed {
+  logic [94:0] m_1;
+  logic m_2;
+} struct96_t;
+
+module t (
+    input struct_t test_input,
+    input struct96_t t96
+);
+endmodule
