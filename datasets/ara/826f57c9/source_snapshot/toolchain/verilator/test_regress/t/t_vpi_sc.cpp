431fe876d8cefedacc18ffe8b53cbee15091039b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:06:10 +0800

    t_vpi_sc.cpp

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_vpi_sc.cpp
@@ -0,0 +1,17 @@
+// -*- mode: C++; c-file-style: "cc-mode" -*-
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+#include VM_PREFIX_INCLUDE
+
+VM_PREFIX* tb = nullptr;
+
+int sc_main(int argc, char* argv[]) {
+    tb = new VM_PREFIX{"tb"};
+
+    VL_PRINTF("*-* All Finished *-*\n");
+    tb->final();
+    VL_DO_DANGLING(delete tb, tb);
+    return 0;
+}
