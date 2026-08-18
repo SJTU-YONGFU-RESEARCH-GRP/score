9e062b25561b5d6832836b0b0fa87d778a5e9e7d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:06:11 +0800

    t_vpi_sc.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_vpi_sc.v
@@ -0,0 +1,19 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of either the GNU Lesser General Public License Version 3
+// or the Perl Artistic License Version 2.0.
+// SPDX-FileCopyrightText: 2010 Wilson Snyder
+// SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+module t;
+
+  // bug1081 - We don't use VPI, just need SC with VPI
+
+  initial begin
+    $write("%0t: Hello\n", $time);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule : t
