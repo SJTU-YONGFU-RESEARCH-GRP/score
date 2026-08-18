0ce7a6209c38ecb9f4178bb267252fdd5db27a33
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:09:16 +0800

    t_concat_large_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_concat_large_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2015 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  wire [32767:0] a = {32768{1'b1}};
+  wire [32767:0] b = '0;
+
+  initial begin
+    $stop;
+  end
+
+endmodule
