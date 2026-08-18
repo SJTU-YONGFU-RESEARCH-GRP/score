661251434cc0f71cb5f7a0b156cc221ab50920f1
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:06:55 +0800

    t_with_suggest_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_with_suggest_bad.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module for SystemVerilog 'alias'
+//
+// Simple bi-directional alias test.
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  initial begin
+    int q[$];
+    int qv[$];  // Value returns
+    q = '{1, 2, 2, 4, 3};
+
+    qv = q.find with (itemm == 2);
+
+    qv = q.find(misspelled) with (misspelledd == 2);
+  end
+
+endmodule
