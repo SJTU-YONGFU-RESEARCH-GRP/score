2b236428ce717453c8d9251af75b9f64e9a0d6d1
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:21:06 +0800

    t_fork_jumpblock.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_fork_jumpblock.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+class bar;
+  task foo(logic r);
+    int a, b;
+    if (r) return;
+    fork
+      a = #1 b;
+    join_none
+  endtask
+endclass
+
+module t;
+  bar b = new;
+
+  initial begin
+    b.foo(0);
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
