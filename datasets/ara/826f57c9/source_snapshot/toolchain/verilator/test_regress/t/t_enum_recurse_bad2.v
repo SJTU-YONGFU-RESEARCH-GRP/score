bde8c375e6fd14ea791ceb9037dd80e92f3f4c85
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:16:29 +0800

    t_enum_recurse_bad2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_enum_recurse_bad2.v
@@ -0,0 +1,8 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+typedef enum foo_t;
+typedef enum foo_t { A  = 'b0, B = 'b1 } foo_t;
