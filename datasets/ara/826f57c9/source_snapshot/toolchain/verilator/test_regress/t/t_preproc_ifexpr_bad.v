3d66a5b85650476bec08fa5dd710c27f7f933b98
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:44:23 +0800

    t_preproc_ifexpr_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_preproc_ifexpr_bad.v
@@ -0,0 +1,38 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2024 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// verilog_format: off
+`begin_keywords "1800-2023"
+
+`define ONE
+`undef ZERO
+
+`elsif ( ONE )  // BAD: elsif without if
+`endif
+
+`ifdef ( ) // BAD: Missing value
+`endif
+
+`ifdef ( && ZERO)  // BAD: Expr
+`endif
+
+`ifdef ( ZERO && )  // BAD: Expr
+`endif
+
+`ifdef ( 1 )  // BAD: Constant
+`endif
+
+`ifdef ( ONE & ZERO)  // BAD: Operator
+`endif
+
+`ifdef ( % )  // BAD: % is syntax error
+`endif
+
+`ifdef ) // BAD: ) without (
+`endif
+
+`ifdef ( ONE // BAD: Missing paren
+`endif
