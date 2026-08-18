924870a04bebca1471703ecef5d0d55980feed55
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:58:50 +0800

    t_trace_timescale.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_trace_timescale.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2013 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+`timescale 1ms / 1ms
+
+// See also t_time_sc_*.v/pl
+
+module t (
+    input clk
+);
+
+  integer cyc;
+  initial cyc = 0;
+
+  always @(posedge clk) begin
+    cyc <= cyc + 1;
+    if (cyc == 10) begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+endmodule
