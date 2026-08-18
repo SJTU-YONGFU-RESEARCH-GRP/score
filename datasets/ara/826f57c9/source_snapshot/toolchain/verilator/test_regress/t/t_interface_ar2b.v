2aa488552b67bf6cc2eeea0db6f842a9da76e8e7
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:27:45 +0800

    t_interface_ar2b.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_interface_ar2b.v
@@ -0,0 +1,33 @@
+// DESCRIPTION: Verilator: SystemVerilog interface test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Thierry Tambe
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  sub sub[1] ();
+
+  ahb_slave_intf AHB_S[1] ();
+
+  AHB_MEM uMEM (.S(AHB_S[0]));
+  //   AHB_MEM uMEM(.S(AHB_S[0].source));
+
+endmodule
+
+module sub;
+endmodule
+
+module AHB_MEM (
+    ahb_slave_intf.source S
+);
+
+endmodule
+
+interface ahb_slave_intf ();
+
+  logic [31:0] HADDR;
+
+  modport source(input HADDR);
+
+endinterface
