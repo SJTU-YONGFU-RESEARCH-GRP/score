70e067fbfb2dc147705659d0de93ce7dcfb9e201
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:31:05 +0800

    t_langext_3.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_langext_3.v
@@ -0,0 +1,22 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// A test of the +verilog2001ext+ and +verilog2005ext+ flags.
+//
+// This source code uses the uwire declaration, which is only valid in Verilog
+// 2005.
+//
+// Compile only test, so no need for "All Finished" output.
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2012 Jeremy Bennett
+// SPDX-License-Identifier: CC0-1.0
+
+module t (  /*AUTOARG*/
+    // Inputs
+    clk
+);
+  input clk;
+
+  uwire w;  // Only in Verilog 2005
+
+endmodule
