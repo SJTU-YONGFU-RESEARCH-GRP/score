1bbc507c225b301eeba55376c0852697776127aa
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:07:44 +0800

    t_class_super_bad2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_super_bad2.v
@@ -0,0 +1,12 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Rafal Kapuscik
+// SPDX-License-Identifier: CC0-1.0
+//
+
+class Cls;
+  task t;
+    super.i = 1;  // Bad - no extends
+  endtask
+endclass
