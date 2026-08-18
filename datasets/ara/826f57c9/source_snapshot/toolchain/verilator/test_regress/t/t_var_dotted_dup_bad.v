637d72126e6b12f64f926299e0cb2814e24cb015
yongfu <liyongfu.sg@gmail.com>
2026-08-18 13:03:05 +0800

    t_var_dotted_dup_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_var_dotted_dup_bad.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2020 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  generate
+    begin
+      eh2_ram dccm_bank (.*);
+    end
+    begin
+      eh2_ram dccm_bank (.*);  // Error: duplicate
+    end
+  endgenerate
+
+endmodule
+
+module eh2_ram ();
+endmodule
