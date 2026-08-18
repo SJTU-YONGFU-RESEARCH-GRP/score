f5e64130bf86f6990ef0b6f997245654013f3de7
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:29:22 +0800

    t_interface_modport_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_interface_modport_bad.v
@@ -0,0 +1,27 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2013 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+interface ifc;
+  integer ok;
+  modport out_modport(output ok);
+endinterface
+
+module t;
+
+  ifc itop ();
+
+  counter_ansi c1 (
+      .isub(itop),
+      .i_value(4'h4)
+  );
+
+endmodule
+
+module counter_ansi (
+    ifc.oop_modport isub,  // Bad
+    input logic [3:0] i_value
+);
+endmodule
