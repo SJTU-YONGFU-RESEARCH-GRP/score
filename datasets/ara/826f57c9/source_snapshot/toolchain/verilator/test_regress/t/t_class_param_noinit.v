75d4231a3ecde4fdad4b0192e373596c48b5b7be
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:07:05 +0800

    t_class_param_noinit.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_param_noinit.v
@@ -0,0 +1,20 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+// No init value is legal with classes, as long as not used without the parameter
+class Cls #(
+    int A,
+    int B
+);
+endclass
+
+module t;
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
