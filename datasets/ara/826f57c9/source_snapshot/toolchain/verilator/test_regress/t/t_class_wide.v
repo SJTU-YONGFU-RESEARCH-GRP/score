73f068a361f2842fc769d6225d15008b67a42309
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:08:08 +0800

    t_class_wide.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_wide.v
@@ -0,0 +1,28 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain
+// SPDX-FileCopyrightText: 2022 Jomit626
+// SPDX-License-Identifier: CC0-1.0
+
+`ifndef WIDTH
+`define WIDTH 128
+`endif
+
+class item;
+  bit [`WIDTH-1:0] data;
+endclass
+
+module t;
+  logic [`WIDTH-1:0] data;
+  item item0 = new;
+
+  initial begin
+    item0.data = `WIDTH'hda7ada7a;
+    data = item0.data;
+
+    if (data != `WIDTH'hda7ada7a) $stop();
+
+    $write("*-* All Finished *-*\n");
+    $finish();
+  end
+endmodule
