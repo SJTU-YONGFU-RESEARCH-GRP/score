cb83a36130480877480445fca29daca715c7273e
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:52:42 +0800

    timescale.sv

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_sv_cpu_code/timescale.sv
@@ -0,0 +1,10 @@
+// DESCRIPTION: Verilator: Large test for SystemVerilog
+
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// SPDX-FileCopyrightText: 2012
+// SPDX-License-Identifier: CC0-1.0
+
+// Contributed by M W Lund, Atmel Corporation.
+
+// **** Set simulation time scale ****
+`timescale 1ns/1ps
