6d3245d63f8e72b21c415cd9ad5f8d0d4c7a8656
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:20:37 +0800

    t_foreach_nindex_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_foreach_nindex_bad.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  int array[2][2];
+
+  initial begin
+    foreach (array[i, j, badk, badl]);  // bad
+
+    $stop;
+  end
+
+endmodule
