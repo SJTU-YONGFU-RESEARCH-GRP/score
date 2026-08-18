9def0a9b52ab424781f4c2a4209e794596105aca
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:44:10 +0800

    t_preproc_elsif_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_preproc_elsif_bad.v
@@ -0,0 +1,18 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2010 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+//See bug289
+// verilog_format: off
+`elsif A
+`endif
+
+`else
+`endif
+
+`error `include
+
+module t;
+endmodule
