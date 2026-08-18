ef5676c989b59c16d376964fbfa7cf364c378b6b
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:06:44 +0800

    t_class_param_bad2.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_class_param_bad2.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2022 Antmicro Ltd
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls #(
+    type PARAMB
+);
+endclass
+
+module t;
+
+  Cls c;  // Missing type param
+
+endmodule
