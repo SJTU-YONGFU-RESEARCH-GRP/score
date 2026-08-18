bce6736356225a0b52bbe608dd2bb999d90346dc
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:41:09 +0800

    t_param_circ_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_circ_bad.v
@@ -0,0 +1,15 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2011 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  sub sub ();
+endmodule
+
+module sub #(
+    parameter WIDTH = X,
+    parameter X = WIDTH
+) ();
+endmodule
