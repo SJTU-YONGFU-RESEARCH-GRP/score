d0bc62c0e4e583c1ab34a4e782ef63013b1fe46c
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:42:29 +0800

    t_param_width_loc_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_param_width_loc_bad.v
@@ -0,0 +1,21 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2019 Driss Hafdi
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+
+  // bug1624
+  test #(.PARAM(32'd0)) test_i ();
+
+  initial begin
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+endmodule
+
+module test #(
+    parameter logic PARAM = 1'b0
+) ();
+endmodule
