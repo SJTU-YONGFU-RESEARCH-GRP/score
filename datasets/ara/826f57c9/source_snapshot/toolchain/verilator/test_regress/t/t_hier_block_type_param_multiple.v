732c47aa950c8d939ae4f4fcc7a989ea7ff4d9c4
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:07 +0800

    t_hier_block_type_param_multiple.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_hier_block_type_param_multiple.v
@@ -0,0 +1,41 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain
+// SPDX-FileCopyrightText: 2024 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+  logic [31:0] in1;
+  logic [31:0] out1;
+
+  assign in1 = 0;
+
+  Test #(
+      .TYPE_IN(logic [31:0]),
+      .TYPE_OUT(logic [31:0])
+  ) test (
+      .out(out1),
+      .in(in1)
+  );
+
+  always @(posedge clk) begin
+    if (out1 !== ~in1) $stop;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
+
+endmodule
+
+module Test #(
+    parameter type TYPE_IN = logic [4:0],
+    parameter type TYPE_OUT = logic [7:0]
+) (
+    output TYPE_IN out,
+    input TYPE_OUT in
+);
+  /*verilator hier_block*/
+
+  assign out = ~in;
+endmodule
