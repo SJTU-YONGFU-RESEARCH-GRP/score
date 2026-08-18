491d3aab1847e811d66b92e7360b98a97805aef8
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:54:02 +0800

    t_threads_counter.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_threads_counter.v
@@ -0,0 +1,22 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2017 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  int cyc;
+
+  always @(posedge clk) begin
+    cyc <= cyc + 1;
+    if (cyc != 0) begin
+      if (cyc == 10) begin
+        $write("*-* All Finished *-*\n");
+        $finish;
+      end
+    end
+  end
+endmodule
