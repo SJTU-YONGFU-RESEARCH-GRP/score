176a3086c45bafec7c2219315461cc7b24022065
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:24:35 +0800

    t_gen_var_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_gen_var_bad.v
@@ -0,0 +1,13 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2005 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+  integer i;
+  generate
+    for (i = 0; i < 3; i = i + 1) begin  // Bad: i is not a genvar
+    end
+  endgenerate
+endmodule
