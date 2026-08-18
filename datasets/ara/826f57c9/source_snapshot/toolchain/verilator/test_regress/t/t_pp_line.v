59a9840ef66dd97ed4638e177a6bf7fba7ee6d2d
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:43:46 +0800

    t_pp_line.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_pp_line.v
@@ -0,0 +1,17 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Anthony Donlon
+// SPDX-License-Identifier: CC0-1.0
+
+module t;
+`line 100 "some file" 0
+$info("aaaaaaaa file='%s'", `__FILE__);
+$info("bbbbbbbb file='%s'", `__FILE__);
+`line 200 "somefile.v" 0
+$info("cccccccc file='%s'", `__FILE__);
+`line 300 "/a/somefile.v" 0
+$info("dddddddd file='%s'", `__FILE__);
+`line 400 "C:\\a\\somefile.v" 0
+$info("eeeeeeee file='%s'", `__FILE__);
+endmodule
