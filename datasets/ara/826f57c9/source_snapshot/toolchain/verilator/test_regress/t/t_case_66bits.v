2ae1b0792cf2d74361fecfb7b53aea8635e9c8f9
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:03:41 +0800

    t_case_66bits.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_case_66bits.v
@@ -0,0 +1,25 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2005 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module t (
+    input clk
+);
+
+  reg [65:0] idx  /*verilator public*/;
+  initial idx = 1;
+
+  always @(posedge clk) begin
+    case (idx)
+      1: idx = 100;
+      100: begin
+        $write("*-* All Finished *-*\n");
+        $finish;
+      end
+      default: $stop;
+    endcase
+  end
+
+endmodule
