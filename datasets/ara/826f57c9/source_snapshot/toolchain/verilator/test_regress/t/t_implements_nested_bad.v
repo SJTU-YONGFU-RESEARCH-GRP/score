512bddc40fa79e81fcb08251704db9b1e847d173
yongfu <liyongfu.sg@gmail.com>
2026-08-18 12:25:38 +0800

    t_implements_nested_bad.v

---
+++ datasets/ara/826f57c9/source_snapshot/toolchain/verilator/test_regress/t/t_implements_nested_bad.v
@@ -0,0 +1,16 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2023 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+class Cls;
+  interface class inte;
+    interface class bad_cannot_nest;
+    endclass
+  endclass
+endclass
+
+module t;
+  Cls c;
+endmodule
